915a2b67d5e15a06802d46d3b87f3341287d9318
unknown <ouyh19@189.cn>
2015-11-14 23:20:36 +0800

    2015-11-14 update college precise kpi views and scripts

---
+++ LtePlatform/Scripts/college/precise.controller.js
@@ -0,0 +1,55 @@
+﻿var queryCells = function () {
+    sendRequest("/api/CollegePrecise/", "GET", {
+        collegeName: app.selectedCollege(),
+        begin: app.startDate(),
+        end: app.endDate()
+    }, function (data) {
+        app.cellList(data);
+    });
+};
+
+var queryPrecise = function (cell) {
+    var chart = new comboChart();
+    chart.title.text = cell.eNodebName + "-" + cell.sectorId + '精确覆盖率变化趋势';
+    var data = {
+        cellId: cell.eNodebId,
+        sectorId: cell.sectorId,
+        begin: app.startDate(),
+        end: app.endDate()
+    };
+    var dom = {
+        tag: "#dialog-modal",
+        width: 900,
+        height: 480
+    };
+    var dates = [];
+    var firstRates = [];
+    var secondRates = [];
+    var thirdRates = [];
+    var mrs = [];
+    var setting = this.setting;
+    $.ajax({
+        url: "/api/QueryPreciseStat/",
+        type: "GET",
+        dataType: "json",
+        data: data,
+        success: function (result) {
+            $(result).each(function (index) {
+                dates.push(result[index].DateString);
+                mrs.push(result[index].TotalMrs);
+                firstRates.push(result[index].FirstRate);
+                secondRates.push(result[index].SecondRate);
+                thirdRates.push(result[index].ThirdRate);
+            });
+            setting.categories = dates;
+            setting.xLabel = "日期";
+            setting.setPrimaryYAxis('次', 'MR数量');
+            setting.addColumnSeries(mrs, 'MR数量', '次', 0);
+            setting.addYAxis('叠重覆盖率', '%', 2);
+            setting.addLineSeries(firstRates, "第一邻区重叠覆盖率(%)", '%', 1);
+            setting.addLineSeries(secondRates, "第二邻区重叠覆盖率(%)", '%', 1);
+            setting.addLineSeries(thirdRates, "第三邻区重叠覆盖率(%)", '%', 1);
+            $(dom.tag).showChartDialog(setting.title, setting.getOptions(), dom.width, dom.height);
+        }
+    });
+};
---
+++ LtePlatform/Scripts/college/precise.viewmodel.js
@@ -0,0 +1,35 @@
+﻿function PreciseViewModel(app, dataModel) {
+    var self = this;
+
+    app.colleges = ko.observableArray([]);
+    app.selectedCollege = ko.observable();
+    app.startDate = ko.observable((new Date()).getDateFromToday(-7).Format("yyyy-MM-dd"));
+    app.endDate = ko.observable((new Date()).getDateFromToday(-1).Format("yyyy-MM-dd"));
+    app.cellList = ko.observableArray([]);
+
+    app.initialize = function () {
+        $("#StartDate").datepicker({ dateFormat: 'yy-mm-dd' });
+        $("#EndDate").datepicker({ dateFormat: 'yy-mm-dd' });
+
+        $.ajax({
+            method: 'get',
+            url: app.dataModel.collegeQueryUrl,
+            contentType: "application/json; charset=utf-8",
+            headers: {
+                'Authorization': 'Bearer ' + app.dataModel.getAccessToken()
+            },
+            success: function (data) {
+                app.colleges.removeAll();
+                for (var i = 0; i < data.length; i++) {
+                    app.colleges.push(data[i].name);
+                }
+            }
+        });
+    };
+}
+
+app.addViewModel({
+    name: "Precise",
+    bindingMemberName: "precise",
+    factory: PreciseViewModel
+});
