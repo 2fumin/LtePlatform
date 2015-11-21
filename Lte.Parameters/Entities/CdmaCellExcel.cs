2449b58989b8d1aca878f9d840e184b2abea4bce
ouyh18 <ouyh18@189.cn>
2015-11-21 14:26:11 +0800

    2015-11-21 add the cell classes to import form excel tables.

---
+++ Lte.Parameters/Entities/CdmaCellExcel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lte.Domain.Regular;
+
+namespace Lte.Parameters.Entities
+{
+    public class CdmaCellExcel
+    {
+        [CloneProtection]
+        [ExcelColumn("CELL_ID")]
+        public byte SectorId { get; set; }
+
+        [CloneProtection]
+        [ExcelColumn("频点")]
+        public int Frequency { get; set; }
+
+        [ExcelColumn("是否接室分")]
+        public string IsIndoor { get; set; } = "否";
+
+        [ExcelColumn("经度")]
+        public double Longtitute { get; set; }
+
+        [ExcelColumn("纬度")]
+        public double Lattitute { get; set; }
+
+        [ExcelColumn("天线挂高")]
+        public double Height { get; set; }
+
+        [ExcelColumn("机械下倾角")]
+        public double MTilt { get; set; }
+
+        [ExcelColumn("电下倾角")]
+        public double ETilt { get; set; }
+
+        [ExcelColumn("方位角")]
+        public double Azimuth { get; set; }
+
+        [ExcelColumn("天线增益")]
+        public double AntennaGain { get; set; }
+
+        [CloneProtection]
+        [ExcelColumn("基站编号")]
+        public int BtsId { get; set; }
+
+        [CloneProtection]
+        [ExcelColumn("小区标识")]
+        public int CellId { get; set; }
+
+        [CloneProtection]
+        [ExcelColumn("载扇类型(1X/DO)")]
+        public string CellType { get; set; }
+
+        [CloneProtection]
+        [ExcelColumn("LAC")]
+        public string Lac { get; set; }
+
+        [CloneProtection]
+        [ExcelColumn("PN码")]
+        public short Pn { get; set; }
+    }
+}
---
+++ Lte.Parameters/Entities/CellExcel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lte.Domain.Regular;
+
+namespace Lte.Parameters.Entities
+{
+    public class CellExcel
+    {
+        [CloneProtection]
+        [ExcelColumn("CELL_ID")]
+        public byte SectorId { get; set; }
+
+        [CloneProtection]
+        [ExcelColumn("频点")]
+        public int Frequency { get; set; }
+
+        [ExcelColumn("是否接室分")]
+        public string IsIndoor { get; set; } = "否";
+
+        [ExcelColumn("经度")]
+        public double Longtitute { get; set; }
+
+        [ExcelColumn("纬度")]
+        public double Lattitute { get; set; }
+
+        [ExcelColumn("天线挂高")]
+        public double Height { get; set; }
+
+        [ExcelColumn("机械下倾角")]
+        public double MTilt { get; set; }
+
+        [ExcelColumn("电下倾角")]
+        public double ETilt { get; set; }
+
+        [ExcelColumn("方位角")]
+        public double Azimuth { get; set; }
+
+        [ExcelColumn("天线增益")]
+        public double AntennaGain { get; set; }
+
+        [ExcelColumn("eNodeB ID")]
+        public int ENodebId { get; set; }
+
+        [ExcelColumn("频段号")]
+        public byte BandClass { get; set; }
+
+        [ExcelColumn("天线信息")]
+        public string AntennaInfo { get; set; }
+
+        [ExcelColumn("收发类型")]
+        public string TransmitReceive { get; set; }
+
+        [ExcelColumn("共天线信息")]
+        public string ShareCdmaInfo { get; set; }
+
+        [ExcelColumn("PCI")]
+        public short Pci { get; set; }
+
+        [ExcelColumn("根序列索引")]
+        public short Prach { get; set; }
+
+        [ExcelColumn("TAC")]
+        public int Tac { get; set; }
+
+        [ExcelColumn("参考信号功率")]
+        public double RsPower { get; set; }
+
+        [ExcelColumn("C网共站小区ID")]
+        public string CdmaCellId { get; set; }
+    }
+}
