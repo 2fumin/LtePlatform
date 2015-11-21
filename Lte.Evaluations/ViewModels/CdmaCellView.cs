aff3d23e881fe596305e2a6e381dfc3ec6f8104b
ouyh18ctc <ouyh19@189.cn>
2015-11-21 23:28:40 +0800

    2015-11-21 update the definitions of CdmaBts and CdmaCellView.

---
+++ Lte.Evaluations/ViewModels/CdmaCellView.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lte.Parameters.Entities;
+using Lte.Parameters.Abstract;
+using Lte.Domain.Regular;
+
+namespace Lte.Evaluations.ViewModels
+{
+    public class CdmaCellView
+    {
+        public string BtsName { get; set; }
+
+        public int BtsId { get; set; } = -1;
+
+        public byte SectorId { get; set; } = 31;
+
+        public string CellType { get; set; } = "DO";
+
+        public int Frequency { get; set; } = 0;
+
+        public int CellId { get; set; }
+
+        public string Lac { get; set; }
+
+        public short Pn { get; set; }
+
+        public double Longtitute { get; set; }
+
+        public double Lattitute { get; set; }
+
+        public double Height { get; set; }
+
+        public double DownTilt { get; set; }
+
+        public double Azimuth { get; set; }
+
+        public double AntennaGain { get; set; }
+
+        public string Indoor { get; set; }
+        
+        public double RsPower { get; set; }
+
+        public string FrequencyList { get; set; }
+
+        public CdmaCellView() { }
+
+        public CdmaCellView(CdmaCell cell, IBtsRepository repository)
+        {
+            cell.CloneProperties(this);
+            CdmaBts bts = repository.FirstOrDefault(x => x.BtsId == cell.BtsId);
+            BtsName = bts == null ? "Undefined" : bts.Name;
+            Indoor = cell.IsOutdoor ? "室外" : "室内";
+            DownTilt = cell.ETilt + cell.MTilt;
+        }
+    }
+}
---
+++ Lte.Parameters/Entities/CdmaBts.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.Domain.Entities;
+using Lte.Domain.Common.Geo;
+
+namespace Lte.Parameters.Entities
+{
+    public class CdmaBts : Entity
+    {
+        public int ENodebId { get; set; } = -1;
+
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        public int TownId { get; set; }
+
+        public double Longtitute { get; set; }
+
+        public double Lattitute { get; set; }
+
+        public double BaiduLongtitute
+        { get { return Longtitute + GeoMath.BaiduLongtituteOffset; } }
+
+        public double BaiduLattitute
+        { get { return Lattitute + GeoMath.BaiduLattituteOffset; } }
+
+        public string Address { get; set; }
+
+        public int BtsId { get; set; }
+
+        public short BscId { get; set; }
+    }
+}
