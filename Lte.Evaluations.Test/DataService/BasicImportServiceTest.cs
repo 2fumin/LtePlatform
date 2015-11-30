ed53ab919e99283566749f1cbf66944d1d92f84a
ouyh18 <ouyh18@189.cn>
2015-11-30 21:24:21 +0800

    2015-11-30 Finish the BasicImportServiceTest in getting the four Excel Lists.

--- Lte.Evaluations.Test/DataService/BasicImportServiceTest.cs
+++ Lte.Evaluations.Test/DataService/BasicImportServiceTest.cs
@@ -52,5 +52,66 @@ namespace Lte.Evaluations.Test.DataService
             var results = _service.GetNewENodebExcels();
             results.Select(x => x.ENodebId).ToArray().ShouldEqual(outputENodebIds);
         }
+
+        [TestCase(new[] { 1, 2, 3, 4 }, new[] { 4 })]
+        [TestCase(new[] { 1, 2 }, new int[] { })]
+        [TestCase(new[] { 1, 2, 4 }, new[] { 4 })]
+        [TestCase(new[] { 1, 2, 3, 4, 5 }, new[] { 4, 5 })]
+        public void Test_GetNewBtsExcels(int[] inputBtsIds, int[] outputBtsIds)
+        {
+            BasicImportService.BtsExcels = inputBtsIds.Select(x => new BtsExcel
+            {
+                BtsId = x,
+                Name = "Bts-" + x
+            }).ToList();
+            var results = _service.GetNewBtsExcels();
+            results.Select(x => x.BtsId).ToArray().ShouldEqual(outputBtsIds);
+        }
+
+        [TestCase(new[] { 1, 2, 3, 4 }, new byte[] { 1, 2, 3, 4 }, new[] { 4 }, new byte[] { 4 })]
+        [TestCase(new[] { 1, 2, 3, 4 }, new byte[] { 1, 2, 3, 3 }, new[] { 4 }, new byte[] { 3 })]
+        [TestCase(new[] { 1, 2, 3, 4 }, new byte[] { 1, 2, 3, 4 }, new[] { 4 }, new byte[] { 4 })]
+        [TestCase(new[] { 1, 2, 3, 3 }, new byte[] { 1, 2, 3, 4 }, new[] { 3 }, new byte[] { 4 })]
+        [TestCase(new[] { 1, 2, 3, 2 }, new byte[] { 1, 2, 3, 4 }, new[] { 2 }, new byte[] { 4 })]
+        [TestCase(new[] { 1, 2, 3, 4 }, new byte[] { 1, 2, 3, 1 }, new[] { 4 }, new byte[] { 1 })]
+        [TestCase(new[] { 1, 2 }, new byte[] { 1, 2 }, new int[] { }, new byte[] { })]
+        [TestCase(new[] { 1, 2, 4 }, new byte[] { 1, 2, 4 }, new[] { 4 }, new byte[] { 4 })]
+        [TestCase(new[] { 1, 2, 3, 4, 5 }, new byte[] { 1, 2, 3, 4, 5 }, new[] { 4, 5 }, new byte[] { 4, 5 })]
+        [TestCase(new[] { 1, 1, 3, 1, 5 }, new byte[] { 1, 1, 3, 4, 5 }, new[] { 1, 5 }, new byte[] { 4, 5 })]
+        public void Test_GetNewCellExcels(int[] inputENodebIds, byte[] inputSectorIds, int[] outputENodebIds,
+            byte[] outputSectorIds)
+        {
+            BasicImportService.CellExcels = inputENodebIds.Select((t, i) => new CellExcel
+            {
+                ENodebId = t,
+                SectorId = inputSectorIds[i]
+            }).ToList();
+            var results = _service.GetNewCellExcels();
+            results.Select(x => x.ENodebId).ToArray().ShouldEqual(outputENodebIds);
+            results.Select(x => x.SectorId).ToArray().ShouldEqual(outputSectorIds);
+        }
+
+        [TestCase(new[] { 1, 2, 3, 4 }, new byte[] { 1, 2, 3, 4 }, new[] { 4 }, new byte[] { 4 })]
+        [TestCase(new[] { 1, 2, 3, 4 }, new byte[] { 1, 2, 3, 3 }, new[] { 4 }, new byte[] { 3 })]
+        [TestCase(new[] { 1, 2, 3, 4 }, new byte[] { 1, 2, 3, 4 }, new[] { 4 }, new byte[] { 4 })]
+        [TestCase(new[] { 1, 2, 3, 3 }, new byte[] { 1, 2, 3, 4 }, new[] { 3 }, new byte[] { 4 })]
+        [TestCase(new[] { 1, 2, 3, 2 }, new byte[] { 1, 2, 3, 4 }, new[] { 2 }, new byte[] { 4 })]
+        [TestCase(new[] { 1, 2, 3, 4 }, new byte[] { 1, 2, 3, 1 }, new[] { 4 }, new byte[] { 1 })]
+        [TestCase(new[] { 1, 2 }, new byte[] { 1, 2 }, new int[] { }, new byte[] { })]
+        [TestCase(new[] { 1, 2, 4 }, new byte[] { 1, 2, 4 }, new[] { 4 }, new byte[] { 4 })]
+        [TestCase(new[] { 1, 2, 3, 4, 5 }, new byte[] { 1, 2, 3, 4, 5 }, new[] { 4, 5 }, new byte[] { 4, 5 })]
+        [TestCase(new[] { 1, 1, 3, 1, 5 }, new byte[] { 1, 1, 3, 4, 5 }, new[] { 1, 5 }, new byte[] { 4, 5 })]
+        public void Test_GetNewCdmaCellExcels(int[] inputBtsIds, byte[] inputSectorIds, int[] outputBtsIds,
+            byte[] outputSectorIds)
+        {
+            BasicImportService.CdmaCellExcels = inputBtsIds.Select((t, i) => new CdmaCellExcel
+            {
+                BtsId = t,
+                SectorId = inputSectorIds[i]
+            }).ToList();
+            var results = _service.GetNewCdmaCellExcels();
+            results.Select(x => x.BtsId).ToArray().ShouldEqual(outputBtsIds);
+            results.Select(x => x.SectorId).ToArray().ShouldEqual(outputSectorIds);
+        }
     }
 }
