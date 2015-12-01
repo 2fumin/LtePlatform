d18482af352500d3e607d95effb9666dc72c86d9
ouyh18ctc <ouyh19@189.cn>
2015-12-01 14:51:28 +0800

    2015-12-01 Update the basicImport views and scripts.

--- LtePlatform/App_Start/BundleConfig.cs
+++ LtePlatform/App_Start/BundleConfig.cs
@@ -141,6 +141,14 @@ namespace LtePlatform
                 "~/Scripts/college/precise.controller.js",
                 "~/Scripts/app/_run.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/parametersBasicImport").Include(
+                "~/Scripts/sammy-{version}.js",
+                "~/Scripts/app/common.js",
+                "~/Scripts/app/app.datamodel.js",
+                "~/Scripts/app/app.viewmodel.js",
+                "~/Scripts/parameters/basicimport.viewmodel.js",
+                "~/Scripts/app/_run.js"));
+
             // 使用要用于开发和学习的 Modernizr 的开发版本。然后，当你做好
             // 生产准备时，请使用 http://modernizr.com 上的生成工具来仅选择所需的测试。
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
--- LtePlatform/Scripts/parameters/basicimport.viewmodel.js
+++ LtePlatform/Scripts/parameters/basicimport.viewmodel.js
@@ -5,10 +5,6 @@
     self.newCells = ko.observableArray([]);
     self.newBtss = ko.observableArray([]);
     self.newCdmaCells = ko.observableArray([]);
-    self.newENodebsVisible = ko.observable(false);
-    self.newCellsVisible = ko.observable(false);
-    self.newBtssVisible = ko.observable(false);
-    self.newCdmaCellsVisible = ko.observable(false);
     self.newENodebsImport = ko.observable(true);
     self.newCellsImport = ko.observable(true);
     self.newBtssImport = ko.observable(true);
@@ -19,18 +15,12 @@
             sendRequest(app.dataModel.newENodebExcelsUrl, "GET", null, function(data) {
                 self.newENodebs(data);
             });
-        });
-        this.get('#basicImport', function () {
             sendRequest(app.dataModel.newCellExcelsUrl, "GET", null, function (data) {
                 self.newCells(data);
             });
-        });
-        this.get('#basicImport', function () {
             sendRequest(app.dataModel.newBtsExcelsUrl, "GET", null, function (data) {
                 self.newBtss(data);
             });
-        });
-        this.get('#basicImport', function () {
             sendRequest(app.dataModel.newCdmaCellExcelsUrl, "GET", null, function (data) {
                 self.newCdmaCells(data);
             });
