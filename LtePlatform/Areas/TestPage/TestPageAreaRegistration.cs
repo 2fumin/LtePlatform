ad5c48e2d8b3ecc8ffe433ebd6cf325640598423
ouyh18ctc <ouyh19@189.cn>
2015-12-05 15:11:17 +0800

    2015-12-05 Initialize the TestPage Area.

---
+++ LtePlatform/Areas/TestPage/TestPageAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace LtePlatform.Areas.TestPage
+{
+    public class TestPageAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "TestPage";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "TestPage_default",
+                "TestPage/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
