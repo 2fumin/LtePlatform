e113511ed074af4b0ee7a146fe25d9d42455e69f
ouyh18ctc <ouyh19@189.cn>
2015-12-05 15:13:33 +0800

    2015-12-05 Update the views and scripts

---
+++ LtePlatform/Areas/TestPage/Controllers/QUnitTestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LtePlatform.Areas.TestPage.Controllers
+{
+    public class QUnitTestController : Controller
+    {
+        // GET: TestPage/QUnitTest
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
