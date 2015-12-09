df0510a57b3fea235353037aac871ba52bcbf8bf
ouyh18 <ouyh18@189.cn>
2015-12-09 22:15:12 +0800

    2015-12-09 Improve the display of the colleges from reading the html vilew.

--- LtePlatform/Scripts/baidu/college.helper.js
+++ LtePlatform/Scripts/baidu/college.helper.js
@@ -1,19 +1,18 @@
 ﻿var addOneCollegeMarkerInfo = function (data) {
     var marker = new BMap.Marker(new BMap.Point(centerxs[data.id], centerys[data.id]));
-    var html = '<div class="infoBoxContent">'
-        + '<div class="title"><strong>校园基本信息: </strong></div>'
-        + '<div class="list"><ul>'
-        + '<li><div class="left">校园名称:</div><div class="rmb"> ' + data.name
-        + '</div></li><li><div class="left">用户数:</div><div class="rmb"> ' + data.expectedSubscribers
-        + '</div></li><li><div class="left">区域面积: </div><div class="rmb">' + data.area
-        + '</div></li><li><div class="left">LTE基站数: </div><div class="rmb">' + data.totalLteENodebs
-        + '</div></li><li><div class="left">LTE小区数: </div><div class="rmb">' + data.totalLteCells
-        + '</div></li><li><div class="left">CDMA基站数: </div><div class="rmb">' + data.totalCdmaBts
-        + '</div></li><li><div class="left">CDMA小区数: </div><div class="rmb">' + data.totalCdmaCells
-        + '</div></li><li><div class="left">LTE室内分布数: </div><div class="rmb">' + data.totalLteIndoors
-        + '</div></li><li><div class="left">CDMA室内分布数: </div><div class="rmb">' + data.totalCdmaIndoors
-        + '</div></li>'
-        + '</ul></div>'
-        + '</div>';
+    var html = generateCollegeInfoHtml(data);
     addOneMarker(marker, html);
+};
+
+var generateCollegeInfoHtml = function(data) {
+    $("#college-name").html(data.name);
+    $("#college-subscribers").html(data.expectedSubscribers);
+    $("#college-area").html(data.area.toFixed(2));
+    $("#college-lte-enodebs").html(data.totalLteENodebs);
+    $("#college-lte-cells").html(data.totalLteCells);
+    $("#college-cdma-btss").html(data.totalCdmaBts);
+    $("#college-cdma-cells").html(data.totalCdmaCells);
+    $("#college-lte-distributions").html(data.totalLteIndoors);
+    $("#college-cdma-distributions").html(data.totalCdmaIndoors);
+    return $("#college-info-box").html();
 };
