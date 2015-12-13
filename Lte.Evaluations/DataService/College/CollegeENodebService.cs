d8b27de9959ccf085dd132081d5b23259e054945
ouyh18ctc <ouyh19@189.cn>
2015-12-13 17:15:31 +0800

    2015-12-13 Update CollegeENodebService.

--- Lte.Evaluations/DataService/College/CollegeENodebService.cs
+++ Lte.Evaluations/DataService/College/CollegeENodebService.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using AutoMapper;
 using Lte.Evaluations.ViewModels;
 using Lte.Parameters.Abstract;
+using Lte.Parameters.Entities;
 
 namespace Lte.Evaluations.DataService.College
 {
@@ -35,5 +37,13 @@ namespace Lte.Evaluations.DataService.College
             return ids.Select(_eNodebRepository.Get
                 ).Where(eNodeb => eNodeb != null).ToList().Select(x => x.Name);
         } 
+
+        public IEnumerable<ENodebView> QueryCollegeENodebs(IEnumerable<string> collegeNames)
+        {
+            var concateIds = collegeNames.Select(x => _repository.GetENodebIds(x));
+            var distictIds = concateIds.Aggregate((x, y) => x.Concat(y)).Distinct();
+            var items = distictIds.Select(_eNodebRepository.Get).Where(eNodeb => eNodeb != null).ToList();
+            return Mapper.Map<List<ENodeb>, IEnumerable<ENodebView>>(items);
+        }
     }
 }
