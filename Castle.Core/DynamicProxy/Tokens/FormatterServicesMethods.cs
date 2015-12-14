21e19d775ec02126d2daefdaa8c6a5b025cb52fa
ouyh18 <ouyh18@189.cn>
2015-12-14 23:13:22 +0800

    2015-12-14 Add Castle.Windsor project.

--- Castle.Core/DynamicProxy/Tokens/FormatterServicesMethods.cs
+++ Castle.Core/DynamicProxy/Tokens/FormatterServicesMethods.cs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#define FEATURE_SERIALIZATION
+
 #if FEATURE_SERIALIZATION
 
 namespace Castle.DynamicProxy.Tokens
