d646bb64c7714368ee05f046c6843d2443fdcbcd
ouyh18ctc <ouyh19@189.cn>
2015-12-19 21:01:55 +0800

    2015-12-19 Finish the query of totalMrs given the date span with respect to districts.

--- LtePlatform/Scripts/kpi/precise.trend.viewmodel.js
+++ LtePlatform/Scripts/kpi/precise.trend.viewmodel.js
@@ -7,6 +7,7 @@
     self.endDate = ko.observable((new Date()).getDateFromToday(-1).Format("yyyy-MM-dd"));
     self.districts = ko.observableArray([]);
     self.mrStats = ko.observableArray([]);
+    self.preciseStats = ko.observableArray([]);
 
     Sammy(function () {
         this.get('#preciseTrend', function () {
@@ -20,18 +21,44 @@
 
     self.showKpi = function () {
         sendRequest(app.dataModel.cityListUrl, "GET", {
-            city: self.currentCity
+            city: self.currentCity()
         }, function(result) {
-            result.push(self.currentCity);
+            result.push(self.currentCity());
             self.districts(result);
         });
     };
 
     self.showTrend = function () {
-        self.mrStats([
-            { statDate: '2015-1-1', values: [1, 2] },
-            { statDate: '2015-1-2', values: [3, 4] }
-        ]);
+        self.mrStats([]);
+        sendRequest(app.dataModel.preciseRegionUrl, "GET", {
+            begin: self.beginDate,
+            end: self.endDate,
+            city: self.currentCity
+        }, function (result) {
+            for (var i = 0; i < result.length; i++) {
+                var districtViews = result[i].districtPreciseViews;
+                var statDate = result[i].statDate;
+                var totalMrs = 0;
+                var districtMrStats = [];
+                for (var j = 0; j < self.districts().length - 1; j++) {
+                    var currentDistrictMrs = 0;
+                    for (var k = 0; k < districtViews.length; k++) {
+                        var view = districtViews[k];
+                        if (view.district === self.districts()[j]) {
+                            currentDistrictMrs = view.totalMrs;
+                            totalMrs += currentDistrictMrs;
+                            break;
+                        }
+                    }
+                    districtMrStats.push(currentDistrictMrs);
+                }
+                districtMrStats.push(totalMrs);
+                self.mrStats.push({
+                    statDate: statDate,
+                    values: districtMrStats
+                });
+            }
+        });
     };
 
     return self;
