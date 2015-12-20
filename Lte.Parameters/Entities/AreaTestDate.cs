911bde0339046f8a30afd80c4716d006cfd1de6d
ouyh18 <ouyh18@189.cn>
2015-12-20 22:30:10 +0800

    2015-12-20 Add some DT database table.

---
+++ Lte.Parameters/Entities/AreaTestDate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lte.Parameters.Entities
+{
+    [Table("areaTestDate")]
+    public class AreaTestDate
+    {
+        [Column("area")]
+        [MaxLength(50)]
+        public string Area { get; set; }
+
+        [Column("latestTestDate2G")]
+        [MaxLength(100)]
+        public string LatestTestDate2G { get; set; }
+
+        [Column("latestTestDate3G")]
+        [MaxLength(100)]
+        public string LatestTestDate3G { get; set; }
+
+        [Column("latestTestDate4G")]
+        [MaxLength(100)]
+        public string LatestTestDate4G { get; set; }
+    }
+}
---
+++ Lte.Parameters/Entities/CsvFilesInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lte.Parameters.Entities
+{
+    [Table("csvFilesInfo")]
+    public class CsvFilesInfo
+    {
+        [Column("testDate")]
+        public DateTime? TestDate { get; set; }
+
+        [Column("csvFileName")]
+        public string csvFileName { get; set; }
+
+        [Column("direct")]
+        public string Directory { get; set; }
+
+        [Column("dataType")]
+        public string DataType { get; set; }
+
+        [Column("distance")]
+        public double? Distance { get; set; }
+    }
+}
