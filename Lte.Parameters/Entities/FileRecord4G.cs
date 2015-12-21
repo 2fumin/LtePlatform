f83aef73c1db09bc365fd8dc7747eb9d0dc7ac52
ouyh18 <ouyh18@189.cn>
2015-12-21 17:35:55 +0800

    2015-12-21 Add some parts of FileRecord4G

---
+++ Lte.Parameters/Entities/FileRecord4G.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq.Mapping;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lte.Parameters.Entities
+{
+    public class FileRecord4G
+    {
+        [Column(Name = "ind", DbType = "Int")]
+        public int? Ind { get; set; }
+
+        [Column(Name = "rasterNum", DbType = "SmallInt")]
+        public short RasterNum { get; set; }
+
+        [Column(Name = "rasterNum", DbType = "Char(50)")]
+        public string TestTimeString { get; set; }
+
+        [Column(Name = "lon", DbType = "Float")]
+        public double? Longtitute { get; set; }
+
+        [Column(Name = "lat", DbType = "Float")]
+        public double? Lattitute { get; set; }
+
+        [Column(Name = "eNodeBID", DbType = "Int")]
+        public int? ENodebId { get; set; }
+
+        [Column(Name = "cellID", DbType = "TinyInt")]
+        public byte? SectorId { get; set; }
+
+        [Column(Name = "freq", DbType = "Real")]
+        public double? Frequency { get; set; }
+
+        [Column(Name = "PCI", DbType = "SmallInt")]
+        public short? Pci { get; set; }
+
+        [Column(Name = "RSRP", DbType = "Float")]
+        public double? Rsrp { get; set; }
+    }
+}
