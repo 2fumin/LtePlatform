e56af7039c81a3b96dc5e72d7fcbd7ab4eca56b2
ouyh18 <ouyh18@189.cn>
2015-12-23 20:54:36 +0800

    2015-12-23 Add some documentations

--- Lte.Evaluations/ViewModels/RasterInfoView.cs
+++ Lte.Evaluations/ViewModels/RasterInfoView.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Lte.Domain.Regular;
 using Lte.Parameters.Entities;
 
 namespace Lte.Evaluations.ViewModels
@@ -46,10 +47,13 @@ namespace Lte.Evaluations.ViewModels
         }
     }
 
+    [TypeDoc("测试数据文件网格视图，一个测试数据文件包含的若干个网格")]
     public class FileRasterInfoView
     {
+        [MemberDoc("测试数据文件名")]
         public string CsvFileName { get; set; }
 
+        [MemberDoc("包含的网格编号列表")]
         public IEnumerable<int> RasterNums { get; set; } 
     }
 }
--- Lte.Parameters/Entities/College4GTestResults.cs
+++ Lte.Parameters/Entities/College4GTestResults.cs
@@ -4,27 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Abp.Domain.Entities;
+using Lte.Domain.Regular;
 
 namespace Lte.Parameters.Entities
 {
+    [TypeDoc("校园网4G测试结果")]
     public class College4GTestResults : Entity
     {
+        [MemberDoc("校园编号")]
         public int CollegeId { get; set; }
 
+        [MemberDoc("测试时间")]
         public DateTime TestTime { get; set; }
 
+        [MemberDoc("下载速率")]
         public double DownloadRate { get; set; }
 
+        [MemberDoc("上传速率")]
         public double UploadRate { get; set; }
 
+        [MemberDoc("基站编号")]
         public int ENodebId { get; set; }
 
+        [MemberDoc("扇区编号")]
         public byte SectorId { get; set; }
 
+        [MemberDoc("接入用户数")]
         public int AccessUsers { get; set; }
 
+        [MemberDoc("RSRP")]
         public double Rsrp { get; set; }
 
+        [MemberDoc("SINR")]
         public double Sinr { get; set; }
     }
 }
