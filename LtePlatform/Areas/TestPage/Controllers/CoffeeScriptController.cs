a982266d2a82d2f26b97bcdf719fa9c4d96384fd
ouyh18ctc <ouyh19@189.cn>
2016-01-01 21:15:57 +0800

    2016-1-1 Construct the CoffeeScript test controller.

---
+++ LtePlatform/Areas/TestPage/Controllers/CoffeeScriptController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LtePlatform.Areas.TestPage.Controllers
+{
+    public class CoffeeScriptController : Controller
+    {
+        // GET: TestPage/CoffeeScript
+        public ActionResult Hotseat()
+        {
+            return View();
+        }
+    }
+}
