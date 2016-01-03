f877d08654b6cb93832b67977c1f885d7c65c21e
ouyh18ctc <ouyh19@189.cn>
2016-01-03 15:00:05 +0800

    2016-1-3 Add first angularJS test.

---
+++ LtePlatform/Areas/TestPage/Controllers/AngularTestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LtePlatform.Areas.TestPage.Controllers
+{
+    public class AngularTestController : Controller
+    {
+        // GET: TestPage/AngularTest
+        public ActionResult Simple()
+        {
+            return View();
+        }
+    }
+}
