143ace6ba19f37208b83a72ad1cf26173a1d6c37
ouyh18 <ouyh18@189.cn>
2016-01-03 10:06:48 +0800

    2016-1-3 Fix some bugs.

--- Abp.Tests/Localization/Test_Assembly.cs
+++ Abp.Tests/Localization/Test_Assembly.cs
@@ -17,8 +17,11 @@ namespace Abp.Tests.Localization
             var assmebly = Assembly.GetExecutingAssembly();
             Assert.AreEqual(assmebly.FullName, "Abp.Tests, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null");
             var names = assmebly.GetManifestResourceNames();
-            Assert.AreEqual(names.Length, 1);
+            Assert.AreEqual(names.Length, 4);
             Assert.AreEqual(names[0], "Abp.Tests.Localization.TestResourceFiles.MyTestResource.resources");
+            Assert.AreEqual(names[1], "Abp.Tests.Resources.Embedded.MyResources.js.MyScriptFile1.js");
+            Assert.AreEqual(names[2], "Abp.Tests.Localization.TestXmlFiles.Test-tr.xml");
+            Assert.AreEqual(names[3], "Abp.Tests.Localization.TestXmlFiles.Test.xml");
         }
     }
 }
