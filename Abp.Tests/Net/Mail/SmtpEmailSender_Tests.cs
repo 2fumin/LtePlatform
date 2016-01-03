6e35c89ff97eae081ab862b95bdddf83c7669ae7
ouyh18 <ouyh18@189.cn>
2016-01-03 10:22:56 +0800

    2016-1-3 Add send-email test to Abp.Test.

---
+++ Abp.Tests/Net/Mail/SmtpEmailSender_Tests.cs
@@ -0,0 +1,43 @@
+﻿using Abp.Net.Mail.Smtp;
+using NSubstitute;
+using Xunit;
+
+namespace Abp.Tests.Net.Mail
+{
+    public class SmtpEmailSender_Tests
+    {
+        private readonly SmtpEmailSender _smtpEmailSender;
+
+        public SmtpEmailSender_Tests()
+        {
+            var configuration = Substitute.For<ISmtpEmailSenderConfiguration>();
+
+            configuration.DefaultFromAddress.Returns("smtp.189.cn");
+            configuration.DefaultFromDisplayName.Returns("Ouyang Hui");
+
+            configuration.Host.Returns("smtp.189.cn");
+            configuration.Port.Returns(25);
+
+            //configuration.Domain.Returns("...");
+            configuration.UserName.Returns("ouyh18");
+            configuration.Password.Returns("md@287965");
+
+            //configuration.EnableSsl.Returns(false);
+            //configuration.UseDefaultCredentials.Returns(false);
+
+            _smtpEmailSender = new SmtpEmailSender(configuration);
+        }
+
+        [Fact] //Need to set configuration before executing this test
+        public void Test_Send_Email()
+        {
+            _smtpEmailSender.Send(
+                "ouyh18@189.cn",
+                "ouyh19@189.cn",
+                "Test email", 
+                "An email body",
+                true
+                );
+        }
+    }
+}
