79d22686861c42cfa02964d6323be5a4e43afff9
ouyh18 <ouyh18@189.cn>
2016-01-08 23:04:58 +0800

    2016-1-8 Add the project Moq.Tests.

---
+++ Moq.Tests/ExtensionsFixture.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Moq.Tests
+{
+    public class ExtensionsFixture
+    {
+        #region Public Methods
+
+        [Fact]
+        public void IsMockeableReturnsFalseForValueType()
+        {
+            Assert.False(typeof(int).IsMockeable());
+        }
+
+        [Fact]
+        public void OnceDoesNotThrowOnSecondCallIfCountWasResetBefore()
+        {
+            var mock = new Mock<IFooReset>();
+            mock.Setup(foo => foo.Execute("ping")).Returns("ack");
+
+            mock.Object.Execute("ping");
+            mock.ResetCalls();
+            mock.Object.Execute("ping");
+            mock.Verify(o => o.Execute("ping"), Times.Once());
+        }
+
+        [Fact]
+        public void SetupDoesNotApplyAfterMockWasReset()
+        {
+            var mock = new Mock<IFooReset>();
+            mock.Setup(foo => foo.Execute("ping")).Returns("ack");
+            mock.Reset();
+
+            var result = mock.Object.Execute("ping");
+            Assert.Null(result);
+        }
+        #endregion
+    }
+
+    public interface IFooReset
+    {
+        #region Public Methods
+
+        object Execute(string ping);
+
+        #endregion
+    }
+}
