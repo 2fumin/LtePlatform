4e38492cff1872a2e3028b3a58de464ea4a7b24a
ouyh18 <ouyh18@189.cn>
2016-01-09 23:35:11 +0800

    2016-1-9 Add the applicationUser controller.

---
+++ LtePlatform/Controllers/Account/ApplicationUsersController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using LtePlatform.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace LtePlatform.Controllers.Account
+{
+    public class ApplicationUsersController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var context = ApplicationDbContext.Create();
+            return context.Users.Select(x=>x.UserName);
+        } 
+    }
+}
