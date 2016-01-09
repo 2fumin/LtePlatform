a226a7e21212102a068680b3b8644e29bbe401e6
ouyh18 <ouyh18@189.cn>
2016-01-09 21:28:10 +0800

    2016-1-9 Add ApplicationUsers webapi controller.

---
+++ LtePlatform.Tests/IdentityUser/IdentityUserBasicTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace LtePlatform.Tests.IdentityUser
+{
+    [TestFixture]
+    public class IdentityUserBasicTest
+    {
+        [Test]
+        public void Test_Contruction()
+        {
+            var user = new Microsoft.AspNet.Identity.EntityFramework.IdentityUser("aaa");
+            Assert.AreEqual(user.UserName, "aaa");
+        }
+    }
+}
