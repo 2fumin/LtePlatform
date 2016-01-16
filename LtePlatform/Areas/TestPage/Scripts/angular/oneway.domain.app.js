16bc072db8c4bb07c6f96ec061ffcba734609c2a
ouyh18ctc <ouyh19@189.cn>
2016-01-16 15:38:43 +0800

    2016-01-16 Add one-way and two-way update angularJS test.

--- LtePlatform/Areas/TestPage/Scripts/angular/oneway.domain.app.js
+++ LtePlatform/Areas/TestPage/Scripts/angular/oneway.domain.app.js
@@ -1,16 +1,20 @@
-﻿var app = angular.module("onewayDomainApp", []);
-app.controller('SomeController', function ($scope) {
+﻿app.controller('SomeController', function ($scope) {
     // 反模式，裸值
     $scope.someBareValue = 'hello computer';
+    $scope.someModel = {
+        someValue: 'hello computer 2'
+    }
     // 设置$scope本身的操作，这样没问题
     $scope.someAction = function () {
         // 在SomeController和ChildController内部设置{{ someBareValue }}
         $scope.someBareValue = 'hello human, from parent';
+        $scope.someModel.someValue = 'hello human, from parent 2';
     };
 })
 .controller('ChildController', function ($scope) {
     $scope.childAction = function () {
         // 在ChildController内部设置{{ someBareValue }}
         $scope.someBareValue = 'hello human, from child';
+        $scope.someModel.someValue = 'hello human, from child 2';
     };
 });
---
+++ LtePlatform/Areas/TestPage/Scripts/angular/twoway.domain.app.js
@@ -0,0 +1,14 @@
+﻿app.controller('OtherController', function ($scope) {
+    // 最佳实践，永远使用一个模式
+    $scope.someModel = {
+        someValue: 'hello computer'
+    }
+    $scope.someAction = function () {
+        $scope.someModel.someValue = 'hello human, from parent';
+    };
+})
+.controller('OtherChildController', function ($scope) {
+    $scope.childAction = function () {
+        $scope.someModel.someValue = 'hello human, from child';
+    };
+});
