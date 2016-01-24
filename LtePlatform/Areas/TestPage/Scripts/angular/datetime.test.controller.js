4a1d94675b527f27662c483019749088370468a5
ouyh18ctc <ouyh19@189.cn>
2016-01-24 23:19:26 +0800

    2016-1-24 Construct angularJS datetime picker directive.

---
+++ LtePlatform/Areas/TestPage/Scripts/angular/datetime.test.controller.js
@@ -0,0 +1,22 @@
+﻿app.controller("datetimeTest", function ($scope) {
+    $scope.beginDate = {
+        title: "开始日期",
+        value: (new Date()).getDateFromToday(-7).Format("yyyy-MM-dd")
+    };
+    $scope.endDate = {
+        title: "结束日期",
+        value: (new Date()).Format("yyyy-MM-dd")
+    };
+
+    $('.form_date').datetimepicker({
+        language: 'zh-CN',
+        weekStart: 1,
+        todayBtn: 1,
+        autoclose: 1,
+        todayHighlight: 1,
+        startView: 2,
+        minView: 2,
+        forceParse: 0
+    });
+
+});
---
+++ LtePlatform/Areas/TestPage/Scripts/angular/sidebox.directive.js
@@ -0,0 +1,16 @@
+﻿app.directive('sidebox', function() {
+    return {
+        restrict: 'EA',
+        scope: {
+            title: '@'
+        },
+        transclude: true,
+        template: '<div class="sidebox">\
+            <div class="content">\
+                <h2 class="header">{{ title }}</h2>\
+                <span class="content" ng-transclude>\
+                </span>\
+            </div>\
+        </div>'
+    };
+});
