160885de2f44e4c3c9c795815c26e92f0ef0705f
ouyh18 <ouyh18@189.cn>
2016-01-27 22:19:54 +0800

    2016-1-27 Add some documents.

--- Lte.Evaluations/DataService/InterferenceMatrixView.cs
+++ Lte.Evaluations/DataService/InterferenceMatrixView.cs
@@ -3,27 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Lte.Domain.Regular.Attributes;
 
 namespace Lte.Evaluations.DataService
 {
+    [TypeDoc("干扰菊展视图")]
     public class InterferenceMatrixView
     {
+        [MemberDoc("邻小区PCI")]
         public short DestPci { get; set; }
 
+        [MemberDoc("邻小区基站编号")]
         public int DestENodebId { get; set; }
 
+        [MemberDoc("邻小区扇区编号")]
         public byte DestSectorId { get; set; }
 
+        [MemberDoc("邻小区名称")]
         public string NeighborCellName { get; set; }
 
+        [MemberDoc("模3干扰数")]
         public double Mod3Interferences { get; set; }
 
+        [MemberDoc("模6干扰数")]
         public double Mod6Interferences { get; set; }
 
+        [MemberDoc("6dB干扰数")]
         public double OverInterferences6Db { get; set; }
 
+        [MemberDoc("10dB干扰数")]
         public double OverInterferences10Db { get; set; }
 
+        [MemberDoc("总干扰水平")]
         public double InterferenceLevel { get; set; }
     }
 }
--- LtePlatform/Controllers/Mr/InterferenceNeighborController.cs
+++ LtePlatform/Controllers/Mr/InterferenceNeighborController.cs
@@ -5,9 +5,11 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Lte.Evaluations.DataService;
+using LtePlatform.Models;
 
 namespace LtePlatform.Controllers.Mr
 {
+    [ApiControl("干扰邻区查询控制器")]
     public class InterferenceNeighborController : ApiController
     {
         private readonly InterferenceNeighborService _service;
@@ -18,12 +20,22 @@ namespace LtePlatform.Controllers.Mr
         }
 
         [HttpGet]
+        [ApiDoc("更新指定小区的邻区干扰记录")]
+        [ApiParameterDoc("cellId", "基站编号")]
+        [ApiParameterDoc("sectorId", "扇区编号")]
+        [ApiResponse("更新结果")]
         public int Get(int cellId, byte sectorId)
         {
             return _service.UpdateNeighbors(cellId, sectorId);
         }
 
         [HttpGet]
+        [ApiDoc("查询指定时间段和小区的干扰记录")]
+        [ApiParameterDoc("cellId", "基站编号")]
+        [ApiParameterDoc("sectorId", "扇区编号")]
+        [ApiParameterDoc("begin", "开始日期")]
+        [ApiParameterDoc("end", "结束日期")]
+        [ApiResponse("邻区干扰记录列表")]
         public IEnumerable<InterferenceMatrixView> Get(DateTime begin, DateTime end, int cellId, byte sectorId)
         {
             return _service.QueryViews(begin, end, cellId, sectorId);
