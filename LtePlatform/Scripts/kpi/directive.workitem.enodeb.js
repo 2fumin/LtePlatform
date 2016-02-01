52dde56d348d444f5b915e50d51a7f193165a7af
ouyh18 <ouyh18@189.cn>
2016-02-01 22:10:17 +0800

    2016-2-1 Refactor to ENodeb and Bts angularJS directives.

---
+++ LtePlatform/Scripts/kpi/directive.workitem.enodeb.js
@@ -0,0 +1,30 @@
+﻿app.directive('workitemEnodeb', function() {
+    return {
+        restrict: 'EA',
+        replace: true,
+        template: '<table class="table table-hover">\
+                    <tbody>\
+                        <tr>\
+                            <td>基站名称</td>\
+                            <td>{{eNodebDetails.name}}</td>\
+                            <td>厂家</td>\
+                            <td>{{eNodebDetails.factory}}</td>\
+                            <td>规划编码</td>\
+                            <td>{{eNodebDetails.planNum}}</td>\
+                        </tr>\
+                        <tr>\
+                            <td>经度</td>\
+                            <td>{{eNodebDetails.longtitute}}</td>\
+                            <td>纬度</td>\
+                            <td>{{eNodebDetails.lattitute}}</td>\
+                            <td>开通日期</td>\
+                            <td>{{eNodebDetails.openDate}}</td>\
+                        </tr>\
+                        <tr>\
+                            <td>基站地址</td>\
+                            <td colspan="5">>{{eNodebDetails.address}}</td>\
+                        </tr>\
+                    </tbody>\
+        </table>'
+    };
+});
