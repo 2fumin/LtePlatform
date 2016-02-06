fd14385b68aaf46b100624e303cd2b31b7641f11
ouyh18 <ouyh18@189.cn>
2016-02-07 00:11:49 +0800

    2016-2-7 Finish the angularJS route test.

--- LtePlatform/Scripts/kpi/interferenceimport.viewmodel.js
+++ LtePlatform/Scripts/kpi/interferenceimport.viewmodel.js
@@ -1,4 +1,21 @@
-﻿app.controller("interference.import", function($scope, $http, dumpProgress) {
+﻿app.config([
+    '$routeProvider', function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: '/appViews/FromTxt.html',
+                controller: "interference.import"
+            })
+            .when('/mongo', {
+                templateUrl: '/appViews/FromMongo.html',
+                controller: 'interference.mongo'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    }
+]);
+
+app.controller("interference.import", function ($scope, $http, dumpProgress) {
     $scope.dataModel = new AppDataModel();
     $scope.progressInfo = {
         totalDumpItems: 0,
@@ -17,4 +34,8 @@
     $http.get($scope.dataModel.dumpInterferenceUrl).success(function(result) {
         $scope.progressInfo.totalDumpItems = result;
     });
+});
+
+app.controller('interference.mongo', function($scope, $http) {
+
 });
