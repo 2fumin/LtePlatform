cbcf3babf434dc6b6d01c2f279268f882d2fcbda
ouyh18 <ouyh18@189.cn>
2016-02-14 22:27:42 +0800

    2016-2-14 Refactor simple angularJS test...

---
+++ LtePlatform/Areas/TestPage/Scripts/angular/menu.controller.js
@@ -0,0 +1,25 @@
+﻿app.controller("angular.menu", function ($scope) {
+    var rootPath = '/TestPage/AngularTest/';
+    $scope.menuItems = [
+    {
+        name: "Simple",
+        displayName: "Simple Type Test",
+        url: rootPath + 'Simple'
+    }, {
+        name: "SubmitForm",
+        displayName: "Submit Form",
+        url: rootPath + "SubmitForm"
+    }, {
+        name: "RootProperty",
+        displayName: "Root Property",
+        url: rootPath + "RootProperty"
+    }, {
+        name: "Chapter9Ari",
+        displayName: "第 9 章　内置指令",
+        url: rootPath + "Chapter9Ari"
+    }, {
+        name: "Chapter10Ari",
+        displayName: "第 10 章　指令详解",
+        url: rootPath + "Chapter10Ari"
+    }];
+});
---
+++ LtePlatform/Areas/TestPage/Scripts/angular/simple.controller.js
@@ -0,0 +1,48 @@
+﻿app.controller("simple.root", function($scope) {
+    $scope.pageTitle = "Simple";
+});
+
+var SimpleTypeController = function ($scope) {
+    $scope.simpleA = 1;
+    $scope.simpleB = 2;
+}
+app.controller("SimpleTypeController", SimpleTypeController);
+
+var ClockController = function ($scope, $timeout) {
+    var updateClock = function () {
+        $scope.clock = new Date();
+        $timeout(function () {
+            updateClock();
+        }, 1000);
+    };
+    updateClock();
+};
+app.controller("ClockController", ClockController);
+
+var AddController = function ($scope) {
+    $scope.counter = 0;
+    $scope.add = function (amount) { $scope.counter += amount; };
+    $scope.subtract = function (amount) { $scope.counter -= amount; };
+};
+app.controller("AddController", AddController);
+
+var ParseController = function ($scope, $parse) {
+    $scope.$watch('expr', function (newVal, oldVal, scope) {
+        if (newVal !== oldVal) {
+            var parseFun = $parse(newVal);
+            $scope.parsedValue = parseFun(scope);
+        }
+    });
+};
+app.controller("ParseController", ParseController);
+
+var InterpolateController = function ($scope, $interpolate) {
+    // 设置监听
+    $scope.$watch('emailBody', function (body) {
+        if (body) {
+            var template = $interpolate(body);
+            $scope.previewText = template({ to: $scope.to });
+        }
+    });
+};
+app.controller("InterpolateController", InterpolateController);
