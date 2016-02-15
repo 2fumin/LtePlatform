a59d956f6f55b82804b2efbf145fb5a4bd0658a1
ouyh18 <ouyh18@189.cn>
2016-02-15 14:19:04 +0800

    2016-2-15 Refactor Simple AngularJS test.

--- LtePlatform/Areas/TestPage/Scripts/angular/simple.controller.js
+++ LtePlatform/Areas/TestPage/Scripts/angular/simple.controller.js
@@ -1,14 +1,69 @@
-﻿app.controller("simple.root", function($scope) {
+﻿app.config([
+    '$routeProvider', function ($routeProvider) {
+        var viewDir = "/appViews/Test/Simple/";
+        $routeProvider
+            .when('/', {
+                templateUrl: viewDir+"SimpleType.html",
+                controller: "SimpleTypeController"
+            })
+            .when('/add', {
+                templateUrl: viewDir+"Add.html",
+                controller: "AddController"
+            })
+            .when('/clock', {
+                templateUrl: viewDir+"Clock.html",
+                controller: "ClockController"
+            })
+            .when('/interpolate', {
+                templateUrl: viewDir+"Interpolate.html",
+                controller: "InterpolateController"
+            })
+            .when('/parse', {
+                templateUrl: viewDir + "Parse.html",
+                controller: "ParseController"
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    }
+]);
+
+app.controller("simple.root", function ($scope) {
     $scope.pageTitle = "Simple";
+    var rootPath = "/TestPage/AngularTest/Simple#";
+    $scope.sectionItems = [
+    {
+        name: "Simple",
+        displayName: "简单类型测试",
+        url: rootPath + '/'
+    }, {
+        name: "Add",
+        displayName: "简单加减法",
+        url: rootPath + "/add"
+    }, {
+        name: "Clock",
+        displayName: "时钟控制器",
+        url: rootPath + "/clock"
+    }, {
+        name: "Interpolate",
+        displayName: "插值字符串测试",
+        url: rootPath + "/interpolate"
+    }, {
+        name: "Parse",
+        displayName: "表达式解析测试",
+        url: rootPath + "/parse"
+    }];
 });
 
 var SimpleTypeController = function ($scope) {
+    $scope.sectionTitle = "Simple";
     $scope.simpleA = 1;
     $scope.simpleB = 2;
 }
 app.controller("SimpleTypeController", SimpleTypeController);
 
 var ClockController = function ($scope, $timeout) {
+    $scope.sectionTitle = "Add";
     var updateClock = function () {
         $scope.clock = new Date();
         $timeout(function () {
@@ -20,6 +75,7 @@ var ClockController = function ($scope, $timeout) {
 app.controller("ClockController", ClockController);
 
 var AddController = function ($scope) {
+    $scope.sectionTitle = "Clock";
     $scope.counter = 0;
     $scope.add = function (amount) { $scope.counter += amount; };
     $scope.subtract = function (amount) { $scope.counter -= amount; };
@@ -27,6 +83,7 @@ var AddController = function ($scope) {
 app.controller("AddController", AddController);
 
 var ParseController = function ($scope, $parse) {
+    $scope.sectionTitle = "Interpolate";
     $scope.$watch('expr', function (newVal, oldVal, scope) {
         if (newVal !== oldVal) {
             var parseFun = $parse(newVal);
@@ -37,7 +94,7 @@ var ParseController = function ($scope, $parse) {
 app.controller("ParseController", ParseController);
 
 var InterpolateController = function ($scope, $interpolate) {
-    // 设置监听
+    $scope.sectionTitle = "Parse";
     $scope.$watch('emailBody', function (body) {
         if (body) {
             var template = $interpolate(body);
