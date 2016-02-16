413dc62f8e2f1bdec82d3346f8bc865c6a21cf8a
ouyh18 <ouyh18@189.cn>
2016-02-16 23:07:39 +0800

    2016-2-16 Refactor demo.accordion.js

--- LtePlatform/Areas/TestPage/Scripts/angular/demo.accordion.js
+++ LtePlatform/Areas/TestPage/Scripts/angular/demo.accordion.js
@@ -1,23 +1,26 @@
-function AccordionDemoCtrl($scope) {
-  $scope.oneAtATime = true;
+app.controller('AccordionDemoCtrl', function ($scope) {
+    $scope.oneAtATime = true;
 
-  $scope.groups = [
-    {
-      title: "Dynamic Group Header - 1",
-      content: "Dynamic Group Body - 1"
-    },
-    {
-      title: "Dynamic Group Header - 2",
-      content: "Dynamic Group Body - 2"
-    }
-  ];
+    $scope.groups = [
+      {
+          title: 'Dynamic Group Header - 1',
+          content: 'Dynamic Group Body - 1'
+      },
+      {
+          title: 'Dynamic Group Header - 2',
+          content: 'Dynamic Group Body - 2'
+      }
+    ];
 
-  $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+    $scope.items = ['Item 1', 'Item 2', 'Item 3'];
 
-  $scope.addItem = function() {
-    var newItemNo = $scope.items.length + 1;
-    $scope.items.push('Item ' + newItemNo);
-  };
-}
+    $scope.addItem = function () {
+        var newItemNo = $scope.items.length + 1;
+        $scope.items.push('Item ' + newItemNo);
+    };
 
-app.controller("AccordionDemoCtrl", AccordionDemoCtrl);
+    $scope.status = {
+        isFirstOpen: true,
+        isFirstDisabled: false
+    };
+});
