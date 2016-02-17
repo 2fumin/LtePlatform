a44a66e17bee6683f10e457db337a7bd35ed2943
ouyh18 <ouyh18@189.cn>
2016-02-17 08:38:43 +0800

    2016-2-17 Refactor alert test...

--- LtePlatform/Areas/TestPage/Scripts/angular/demo.alert.js
+++ LtePlatform/Areas/TestPage/Scripts/angular/demo.alert.js
@@ -1,17 +1,14 @@
-function AlertDemoCtrl($scope) {
-  $scope.alerts = [
-    { type: 'warning', msg: 'Oh snap! Change a few things up and try submitting again.' }, 
-    { type: 'success', msg: 'Well done! You successfully read this important alert message.' }
-  ];
+app.controller('AlertDemoCtrl', function ($scope) {
+    $scope.alerts = [
+      { type: 'danger', msg: 'Oh snap! Change a few things up and try submitting again.' },
+      { type: 'success', msg: 'Well done! You successfully read this important alert message.' }
+    ];
 
-  $scope.addAlert = function() {
-    $scope.alerts.push({msg: "Another alert!", type: 'info'});
-  };
+    $scope.addAlert = function () {
+        $scope.alerts.push({ msg: 'Another alert!' });
+    };
 
-  $scope.closeAlert = function(index) {
-    $scope.alerts.splice(index, 1);
-  };
-
-}
-
-app.controller("AlertDemoCtrl", AlertDemoCtrl);
+    $scope.closeAlert = function (index) {
+        $scope.alerts.splice(index, 1);
+    };
+});
