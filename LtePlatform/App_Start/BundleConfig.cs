4880ccff3566e4d8824c3d891647bac6578390f9
ouyh18 <ouyh18@189.cn>
2016-02-17 17:14:29 +0800

    2016-2-17 Bundle the ui-bootstrap scripts.

--- LtePlatform/App_Start/BundleConfig.cs
+++ LtePlatform/App_Start/BundleConfig.cs
@@ -24,6 +24,32 @@ namespace LtePlatform
                 "~/Scripts/knockout-{version}.js",
                 "~/Scripts/knockout.validation.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/ui-bootstraps").Include(
+                "~/Scripts/angular-ui/ui-bootstrap.js",
+                "~/Scripts/angular-ui/uib/accordion.js",
+                "~/Scripts/angular-ui/uib/alert.js",
+                "~/Scripts/angular-ui/uib/buttons.js",
+                "~/Scripts/angular-ui/uib/carousel.js",
+                "~/Scripts/angular-ui/uib/collapse.js",
+                "~/Scripts/angular-ui/uib/dateparser.js",
+                "~/Scripts/angular-ui/uib/datepicker.js",
+                "~/Scripts/angular-ui/uib/debounce.js",
+                "~/Scripts/angular-ui/uib/dropdown.js",
+                "~/Scripts/angular-ui/uib/isClass.js",
+                "~/Scripts/angular-ui/uib/modal.js",
+                "~/Scripts/angular-ui/uib/pager.js",
+                "~/Scripts/angular-ui/uib/pagination.js",
+                "~/Scripts/angular-ui/uib/paging.js",
+                "~/Scripts/angular-ui/uib/popover.js",
+                "~/Scripts/angular-ui/uib/position.js",
+                "~/Scripts/angular-ui/uib/progressbar.js",
+                "~/Scripts/angular-ui/uib/rating.js",
+                "~/Scripts/angular-ui/uib/stackedMap.js",
+                "~/Scripts/angular-ui/uib/tooltip.js",
+                "~/Scripts/angular-ui/uib/tabs.js",
+                "~/Scripts/angular-ui/uib/timepicker.js",
+                "~/Scripts/angular-ui/uib/typeahead.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/highcharts").Include(
                 "~/Scripts/Highcharts/highcharts.src.js",
                 "~/Scripts/Highcharts/highcharts-3d.js",
@@ -246,6 +272,7 @@ namespace LtePlatform
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                 "~/Scripts/bootstrap.js",
+                "~/Content/themes/ui-bootstrap.css",
                 "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
