58eccd6f2f0f9711c831425f84e842f61f5039e3
ouyh18 <ouyh18@189.cn>
2016-02-22 17:27:31 +0800

    2016-2-22 Refactor...

--- LtePlatform/Scripts/rutrace/map.controller.js
+++ LtePlatform/Scripts/rutrace/map.controller.js
@@ -1,3 +1,7 @@
 ﻿app.controller("rutrace.map", function ($scope, baiduMapService) {
     baiduMapService.initializeMap("all-map", 12);
+    $scope.toggleNeighbors = function() {
+        console.log($("#neighbor-info-box").html());
+    };
+    
 });
