08077c642ee310455b684f1332e2be62573fd45f
ouyh18 <ouyh18@189.cn>
2016-02-25 08:47:53 +0800

    2016-2-25 Refactor...

---
+++ LtePlatform/Scripts/parameters/import/bts.lonLat.controller.js
@@ -0,0 +1,9 @@
+﻿app.controller("bts.lonLat", function ($scope, neGeometryService) {
+    $scope.newBtsLonLatEdits
+        = neGeometryService.queryBtsLonLatEdits($scope.importData.newBtss);
+
+    $scope.postBtsLonLat = function () {
+        neGeometryService.mapLonLatEdits($scope.importData.newBtss, $scope.newBtsLonLatEdits);
+    };
+
+});
---
+++ LtePlatform/Scripts/parameters/import/cdmaCell.lonLat.controller.js
@@ -0,0 +1,9 @@
+﻿app.controller("cdmaCell.lonLat", function ($scope, neGeometryService) {
+    $scope.newCdmaCellLonLatEdits
+        = neGeometryService.queryCdmaCellLonLatEdits($scope.importData.newCdmaCells);
+
+    $scope.postCdmaCellLonLat = function () {
+        neGeometryService.mapLonLatEdits($scope.importData.newCdmaCells, $scope.newCdmaCellLonLatEdits);
+    };
+
+});
