8011133d6b424252a661a4035173ff2c3c9a94cd
ouyh18 <ouyh18@189.cn>
2016-02-26 10:35:19 +0800

    2016-2-26 Fix some bugs.

--- LtePlatform/Scripts/rutrace/map.controller.js
+++ LtePlatform/Scripts/rutrace/map.controller.js
@@ -1,6 +1,6 @@
 ﻿app.controller("rutrace.map", function ($scope, $routeParams, $location, baiduMapService, networkElementService) {
     var cell = $scope.topStat.current;
-    $scope.currentSector = {};
+    $scope.preciseSector = {};
     if ($routeParams.name !== cell.eNodebName + "-" + cell.sectorName) {
         if ($scope.topStat.cells[$routeParams.name] === undefined) {
             $location.path('/Rutrace#/top');
@@ -12,11 +12,9 @@
     baiduMapService.initializeMap("all-map", 12);
     networkElementService.queryCellSectors([$scope.topStat.current]).then(function (result) {
         $scope.preciseSector = result[0];
-        console.log($scope.preciseSector);
-        var html = $("#neighbor-info-box").html();
-        console.log(html);
-        baiduMapService.addOneSector(baiduMapService.generateSector($scope.preciseSector), html);
     });
     $scope.toggleNeighbors = function() {
+        var html = $("#sector-info-box").html();
+        baiduMapService.addOneSector(baiduMapService.generateSector($scope.preciseSector), html, "400px");
     };
 });
