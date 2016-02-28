260b7863b7f13a90f208295174cd346cde5d5226
ouyh18 <ouyh18@189.cn>
2016-02-28 10:58:59 +0800

    2016-2-28 Refactor...

--- LtePlatform/Scripts/manage/index.viewmodel.js
+++ LtePlatform/Scripts/manage/index.viewmodel.js
@@ -1,33 +1,47 @@
 ﻿app.config([
-    '$routeProvider', function ($routeProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: '/appViews/Manage/CurrentUser.html',
-                controller: "manage.current"
-            })
-            .when('/all', {
-                templateUrl: '/appViews/Manage/AllUsers.html',
-                controller: 'manage.all'
-            })
-            .when('/roles', {
-                templateUrl: '/appViews/Manage/Roles.html',
-                controller: 'manage.roles'
-            })
-            .when('/roleUser/:name', {
-                templateUrl: '/appViews/Manage/RoleUser.html',
-                controller: 'manage.roleUser'
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
-    }
-]);
-
-app.controller("manage.root", function($scope) {
-    $scope.panelRoot = "账号信息管理";
-    $scope.dataModel = new AppDataModel();
-});
+        '$routeProvider', function($routeProvider) {
+            $routeProvider
+                .when('/', {
+                    templateUrl: '/appViews/Manage/CurrentUser.html',
+                    controller: "manage.current"
+                })
+                .when('/all', {
+                    templateUrl: '/appViews/Manage/AllUsers.html',
+                    controller: 'manage.all'
+                })
+                .when('/roles', {
+                    templateUrl: '/appViews/Manage/Roles.html',
+                    controller: 'manage.roles'
+                })
+                .when('/roleUser/:name', {
+                    templateUrl: '/appViews/Manage/RoleUser.html',
+                    controller: 'manage.roleUser'
+                })
+                .otherwise({
+                    redirectTo: '/'
+                });
+        }
+    ])
+    .run(function($rootScope) {
+        var rootUrl = "/Manage#";
+        $rootScope.menuItems = [
+            {
+                displayName: "本账号信息管理",
+                url: rootUrl + "/"
+            }, {
+                displayName: "所有用户信息管理",
+                url: rootUrl + "/all"
+            }, {
+                displayName: "所有角色管理",
+                url: rootUrl + "/roles"
+            }
+        ];
+        $rootScope.rootPath = rootUrl + "/";
+        $rootScope.page = {
+            title: "本账号信息管理"
+        };
+    });
 
 app.controller("manage.current", function($scope) {
-    $scope.panelTitle = "本用户权限";
+    console.log($scope.page.title);
 });
