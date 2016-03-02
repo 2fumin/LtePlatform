17e7fb86c35c66b81bd7e42c8b8751231acbe9ca
ouyh18ctc <ouyh19@189.cn>
2016-03-02 20:27:30 +0800

    2016-3-2 Finish the diaplay the comments by date time in the workitem details.

--- LtePlatform/Scripts/kpi/workitem/details.controller.js
+++ LtePlatform/Scripts/kpi/workitem/details.controller.js
@@ -5,6 +5,18 @@
     for (var i = 0; i < $scope.viewData.items.length; i++) {
         if ($scope.viewData.items[i].serialNumber === $routeParams.number) {
             $scope.currentView = $scope.viewData.items[i];
+            $scope.platformInfos = [];
+            var comments = $scope.currentView.comments;
+            var fields = comments.split('[');
+            if (fields.length > 1) {
+                for (var j = 1; j < fields.length; j++) {
+                    var subFields = fields[j].split(']');
+                    $scope.platformInfos.push({
+                        time: subFields[0],
+                        contents: subFields[1]
+                    });
+                }
+            }
             break;
         }
     }
