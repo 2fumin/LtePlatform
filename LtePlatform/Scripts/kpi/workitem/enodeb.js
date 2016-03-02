2f237e5b509b7e9b1813b6f9868c7044ab101b01
ouyh18ctc <ouyh19@189.cn>
2016-03-02 19:30:01 +0800

    2016-3-2 Refactor to show workitems relative to one eNodeb.

--- LtePlatform/Scripts/kpi/workitem/enodeb.js
+++ LtePlatform/Scripts/kpi/workitem/enodeb.js
@@ -1,6 +1,9 @@
-﻿app.controller('workitem.eNodeb', function ($scope, networkElementService, $routeParams) {
+﻿app.controller('workitem.eNodeb', function ($scope, networkElementService, $routeParams, workitemService) {
     $scope.serialNumber = $routeParams.serialNumber;
     networkElementService.queryENodebInfo($routeParams.eNodebId).then(function (result) {
         $scope.eNodebDetails = result;
+    });
+    workitemService.queryByENodebId($routeParams.eNodebId).then(function (result) {
+        $scope.viewItems = result;
     });
 });
