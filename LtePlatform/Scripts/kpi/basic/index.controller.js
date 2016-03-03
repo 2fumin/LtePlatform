24e101db28a54195148f2564fea3d9f74dc6ffc2
ouyh18ctc <ouyh19@189.cn>
2016-03-03 08:09:31 +0800

    2016-3-3 Begin kpi basic index.html.

---
+++ LtePlatform/Scripts/kpi/basic/index.controller.js
@@ -0,0 +1,25 @@
+﻿app.controller("kpi.basic", function ($scope, appRegionService, appFormatService) {
+    $scope.page.title = "指标总体情况";
+    var yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+    $scope.statDate = {
+        value: yesterday,
+        opened: false
+    };
+    $scope.city = {
+        selected: "",
+        options: []
+    };
+    $scope.views = {
+        options: ['主要', '2G', '3G'],
+        selected: '主要'
+    };
+    $scope.showKpi = function () {
+
+    };
+    appRegionService.initializeCities()
+        .then(function (result) {
+            $scope.city = result;
+            $scope.showKpi();
+        });
+});
--- LtePlatform/Scripts/kpi/basic/route.controller.js
+++ LtePlatform/Scripts/kpi/basic/route.controller.js
@@ -39,10 +39,10 @@
         var rootUrl = "/Kpi#";
         $rootScope.menuItems = [
             {
-                displayName: "指标总览",
+                displayName: "指标总体情况",
                 url: rootUrl + "/"
             }, {
-                displayName: "变化趋势",
+                displayName: "指标变化趋势",
                 url: rootUrl + "/trend"
             }, {
                 displayName: "所有角色管理",
@@ -51,47 +51,10 @@
         ];
         $rootScope.rootPath = rootUrl + "/";
         $rootScope.page = {
-            title: "工单总览",
+            title: "指标总体情况",
             messages: []
         };
         $rootScope.closeAlert = function (index) {
             $rootScope.page.messages.splice(index, 1);
-        };
-        $rootScope.states = [
-            {
-                name: '未完成'
-            }, {
-                name: '全部'
-            }
-        ];
-        $rootScope.types = [
-            {
-                name: '全部'
-            }, {
-                name: '2/3G'
-            }, {
-                name: '4G'
-            }
-        ];
-        $rootScope.pageSizeSelection = [
-            {
-                value: 10
-            }, {
-                value: 15
-            }, {
-                value: 20
-            }, {
-                value: 30
-            }, {
-                value: 50
-            }
-        ];
-        $rootScope.viewData = {
-            items: [],
-            currentState: $rootScope.states[0],
-            currentType: $rootScope.types[0],
-            itemsPerPage: $rootScope.pageSizeSelection[1],
-            totalItems: 0,
-            currentPage: 1
         };
     });
