3fc69d54056902c2c856372710bdc476866f1982
ouyh18 <ouyh18@189.cn>
2016-03-05 11:23:37 +0800

    2016-3-5 Refactor...

--- LtePlatform/Scripts/kpi/basic/top.drop2G.trend.js
+++ LtePlatform/Scripts/kpi/basic/top.drop2G.trend.js
@@ -1,8 +1,8 @@
 ﻿app.controller('kpi.topDrop2G.trend', function ($scope, $routeParams, appRegionService, appFormatService, kpi2GService) {
     for (var i = 0; i < $scope.topData.drop2G.length; i++) {
         var cell = $scope.topData.drop2G[i];
-        if (cell.cellId === $routeParams.cellId && cell.sectorId === $routeParams.sectorId) {
-            $scope.page.title = "小区" + cell.eNodebName + "-" + cell.sectorId + "指标变化趋势";
+        if (cell.cellId === parseInt($routeParams.cellId) && cell.sectorId === parseInt($routeParams.sectorId)) {
+            $scope.page.title = "小区" + cell.cdmaName + "-" + cell.sectorId + "指标变化趋势";
             break;
         }
     }
