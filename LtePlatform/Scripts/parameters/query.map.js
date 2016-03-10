a2ca9971a9dfbd8578e5fa5985ed7f21a7aa33cb
ouyh18 <ouyh18@189.cn>
2016-03-10 10:18:08 +0800

    2016-3-10 Fix some bugs.

---
+++ LtePlatform/Scripts/parameters/query.map.js
@@ -0,0 +1,39 @@
+﻿app.controller("query.map", function($scope, appRegionService) {
+    $scope.page.title = "小区地图查询";
+    $scope.network = {
+        options: ["LTE", "CDMA"],
+        selected: "LTE"
+    };
+    
+    $scope.updateDistricts = function() {
+        appRegionService.queryDistricts($scope.city.selected).then(function(result) {
+            $scope.district.options = result;
+            $scope.district.selected = result[0];
+        });
+    };
+    $scope.updateTowns = function() {
+        appRegionService.queryTowns($scope.city.selected, $scope.district.selected).then(function(result) {
+            $scope.town.options = result;
+            $scope.town.selected = result[0];
+        });
+    };
+    $scope.queryItems = function() {
+
+    };
+
+    appRegionService.initializeCities().then(function(result) {
+        $scope.city = result;
+        appRegionService.queryDistricts($scope.city.selected).then(function (districts) {
+            $scope.district = {
+                options: districts,
+                selected: districts[0]
+            };
+            appRegionService.queryTowns($scope.city.selected, $scope.district.selected).then(function (towns) {
+                $scope.town = {
+                    options: towns,
+                    selected: towns[0]
+                };
+            });
+        });
+    });
+});
