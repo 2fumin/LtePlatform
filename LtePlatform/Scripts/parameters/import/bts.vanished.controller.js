01d186948d41af82325710ecb07df30757d83320
ouyh18 <ouyh18@189.cn>
2016-03-13 11:37:03 +0800

    2016-3-13 Fix some bugs.

---
+++ LtePlatform/Scripts/parameters/import/bts.vanished.controller.js
@@ -0,0 +1,10 @@
+﻿app.controller('bts.vanished', function ($scope, networkElementService) {
+    $scope.vanishedBtss = [];
+    var data = $scope.importData.vanishedBtsIds;
+    for (var i = 0; i < data.length; i++) {
+        networkElementService.queryBtsInfo(data[i]).then(function (result) {
+            $scope.vanishedBtss.push(result);
+        });
+    }
+
+});
---
+++ LtePlatform/Scripts/parameters/import/cdmaCell.vanished.controller.js
@@ -0,0 +1,10 @@
+﻿app.controller('cdmaCell.vanished', function ($scope, networkElementService) {
+    $scope.vanishedCdmaCells = [];
+    var data = $scope.importData.vanishedCdmaCellIds;
+    for (var i = 0; i < data.length; i++) {
+        networkElementService.queryCdmaCellInfo(data[i].cellId, data[i].sectorId).then(function (result) {
+            $scope.vanishedCdmaCells.push(result);
+        });
+    }
+
+});
