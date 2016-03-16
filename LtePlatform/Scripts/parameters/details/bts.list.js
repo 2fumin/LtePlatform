6d9f1f757d463a9720b2720cd10074c242575fa7
ouyh18 <ouyh18@189.cn>
2016-03-16 10:48:03 +0800

    2016-3-16 Display the eNodeb and bts table info.

--- LtePlatform/Scripts/parameters/details/bts.list.js
+++ LtePlatform/Scripts/parameters/details/bts.list.js
@@ -1,1 +1,6 @@
-﻿app.controller("bts.list", function ($scope, $stateParams) { });
+﻿app.controller("bts.list", function ($scope, $stateParams, networkElementService) {
+    $scope.page.title = $stateParams.city + $stateParams.district + $stateParams.town + "CDMA基站列表";
+    networkElementService.queryBtssInOneTown($stateParams.city, $stateParams.district, $stateParams.town).then(function (result) {
+        $scope.btsList = result;
+    });
+});
--- LtePlatform/Scripts/parameters/details/eNodeb.list.js
+++ LtePlatform/Scripts/parameters/details/eNodeb.list.js
@@ -1,1 +1,6 @@
-﻿app.controller("eNodeb.list", function ($scope, $stateParams) { });
+﻿app.controller("eNodeb.list", function($scope, $stateParams, networkElementService) {
+    $scope.page.title = $stateParams.city + $stateParams.district + $stateParams.town + "LTE基站列表";
+    networkElementService.queryENodebsInOneTown($stateParams.city, $stateParams.district, $stateParams.town).then(function(result) {
+        $scope.eNodebList = result;
+    });
+});
