ac7a09a15daefa1dcb78674161364f97cb2d9a93
ouyh18 <ouyh18@189.cn>
2016-03-16 14:40:59 +0800

    2016-3-16 Add pagination.

--- LtePlatform/Scripts/parameters/details/bts.list.js
+++ LtePlatform/Scripts/parameters/details/bts.list.js
@@ -1,5 +1,6 @@
 ﻿app.controller("bts.list", function ($scope, $stateParams, networkElementService) {
     $scope.page.title = $stateParams.city + $stateParams.district + $stateParams.town + "CDMA基站列表";
+    $scope.currentPage = 1;
     networkElementService.queryBtssInOneTown($stateParams.city, $stateParams.district, $stateParams.town).then(function (result) {
         $scope.btsList = result;
     });
--- LtePlatform/Scripts/parameters/details/eNodeb.list.js
+++ LtePlatform/Scripts/parameters/details/eNodeb.list.js
@@ -1,5 +1,6 @@
 ﻿app.controller("eNodeb.list", function($scope, $stateParams, networkElementService) {
     $scope.page.title = $stateParams.city + $stateParams.district + $stateParams.town + "LTE基站列表";
+    $scope.currentPage = 1;
     networkElementService.queryENodebsInOneTown($stateParams.city, $stateParams.district, $stateParams.town).then(function(result) {
         $scope.eNodebList = result;
     });
