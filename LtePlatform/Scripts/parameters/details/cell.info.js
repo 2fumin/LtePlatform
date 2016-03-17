351767be8e16491859b564bf09ac8ca884b75eed
ouyh18 <ouyh18@189.cn>
2016-03-17 14:12:30 +0800

    2016-3-17 Display the huawei cell info.

--- LtePlatform/Scripts/parameters/details/cell.info.js
+++ LtePlatform/Scripts/parameters/details/cell.info.js
@@ -1,6 +1,15 @@
-﻿app.controller("cell.info", function ($scope, $stateParams, networkElementService) {
+﻿app.controller("cell.info", function ($scope, $stateParams, networkElementService, cellHuaweiMongoService) {
     $scope.page.title = $stateParams.name + "-" + $stateParams.sectorId + "小区信息";
+    $scope.isHuaweiCell = false;
     networkElementService.queryCellInfo($stateParams.eNodebId, $stateParams.sectorId).then(function (result) {
         $scope.lteCellDetails = result;
+    });
+    networkElementService.queryENodebInfo($stateParams.eNodebId).then(function(result) {
+        if (result.factory === '华为') {
+            $scope.isHuaweiCell = true;
+            cellHuaweiMongoService.queryCellParameters($stateParams.eNodebId, $stateParams.sectorId).then(function(info) {
+                $scope.cellMongo = info;
+            });
+        }
     });
 });
---
+++ LtePlatform/Scripts/service/mongo/cell.huawei.js
@@ -0,0 +1,22 @@
+﻿angular.module('huawei.mongo.parameters', ['myApp.url'])
+    .factory('cellHuaweiMongoService', function($q, $http, appUrlService) {
+        return {
+            queryCellParameters: function(eNodebId, sectorId) {
+                var deferred = $q.defer();
+                $http({
+                    method: 'GET',
+                    url: appUrlService.getApiUrl('CellHuaweiMongo'),
+                    params: {
+                        eNodebId: eNodebId,
+                        sectorId: sectorId
+                    }
+                }).success(function (result) {
+                    deferred.resolve(result);
+                })
+                    .error(function (reason) {
+                        deferred.reject(reason);
+                    });
+                return deferred.promise;
+            }
+        };
+    });
