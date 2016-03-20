8a31eea905033bd385d3071328ab0a45feeada1b
ouyh18 <ouyh18@189.cn>
2016-03-20 21:13:30 +0800

    2016-3-20 Display the ENodeb intra-frequency HO parameters.

--- LtePlatform/Scripts/parameters/details/eNodeb.info.js
+++ LtePlatform/Scripts/parameters/details/eNodeb.info.js
@@ -1,9 +1,12 @@
-﻿app.controller("eNodeb.info", function ($scope, $stateParams, networkElementService) {
+﻿app.controller("eNodeb.info", function ($scope, $stateParams, networkElementService, intraFreqHoService) {
     $scope.page.title = $stateParams.name + "基础信息";
     networkElementService.queryENodebInfo($stateParams.eNodebId).then(function (result) {
         $scope.eNodebDetails = result;
     });
     networkElementService.queryCellViewsInOneENodeb($stateParams.eNodebId).then(function(result) {
         $scope.cellList = result;
+    });
+    intraFreqHoService.queryENodebParameters($stateParams.eNodebId).then(function(result) {
+        $scope.intraFreqHo = result;
     });
 });
---
+++ LtePlatform/Scripts/service/mongo/handoff.js
@@ -0,0 +1,38 @@
+﻿angular.module('handoff.parameters', ['myApp.url'])
+    .factory('intraFreqHoService', function($q, $http, appUrlService) {
+        return {
+            queryENodebParameters: function (eNodebId) {
+                var deferred = $q.defer();
+                $http({
+                    method: 'GET',
+                    url: appUrlService.getApiUrl('IntraFreqHo'),
+                    params: {
+                        eNodebId: eNodebId
+                    }
+                }).success(function (result) {
+                    deferred.resolve(result);
+                })
+                    .error(function (reason) {
+                        deferred.reject(reason);
+                    });
+                return deferred.promise;
+            },
+            queryCellParameters: function (eNodebId, sectorId) {
+                var deferred = $q.defer();
+                $http({
+                    method: 'GET',
+                    url: appUrlService.getApiUrl('IntraFreqHo'),
+                    params: {
+                        eNodebId: eNodebId,
+                        sectorId: sectorId
+                    }
+                }).success(function (result) {
+                    deferred.resolve(result);
+                })
+                    .error(function (reason) {
+                        deferred.reject(reason);
+                    });
+                return deferred.promise;
+            }
+        };
+    });
