cca7cfe93833bb579348f892e31c071bb00d249f
ouyh18 <ouyh18@189.cn>
2016-03-20 21:31:04 +0800

    2016-3-20 Add the dbStep32 filter.

--- LtePlatform/Scripts/parameters/details/cell.info.js
+++ LtePlatform/Scripts/parameters/details/cell.info.js
@@ -9,8 +9,6 @@
             $scope.isHuaweiCell = true;
             cellHuaweiMongoService.queryCellParameters($stateParams.eNodebId, $stateParams.sectorId).then(function(info) {
                 $scope.cellMongo = info;
-                $scope.cellMongo.cellSpecificOffsetdB = cellHuaweiMongoService.cellSpecificOffsetDict[info.cellSpecificOffset];
-                $scope.cellMongo.qoffsetFreqdB = cellHuaweiMongoService.cellSpecificOffsetDict[info.qoffsetFreq];
             });
         }
     });
--- LtePlatform/Scripts/service/mongo/cell.huawei.js
+++ LtePlatform/Scripts/service/mongo/cell.huawei.js
@@ -4,22 +4,27 @@
             queryCellParameters: function(eNodebId, sectorId) {
                 var deferred = $q.defer();
                 $http({
-                    method: 'GET',
-                    url: appUrlService.getApiUrl('CellHuaweiMongo'),
-                    params: {
-                        eNodebId: eNodebId,
-                        sectorId: sectorId
-                    }
-                }).success(function (result) {
-                    deferred.resolve(result);
-                })
-                    .error(function (reason) {
+                        method: 'GET',
+                        url: appUrlService.getApiUrl('CellHuaweiMongo'),
+                        params: {
+                            eNodebId: eNodebId,
+                            sectorId: sectorId
+                        }
+                    }).success(function(result) {
+                        deferred.resolve(result);
+                    })
+                    .error(function(reason) {
                         deferred.reject(reason);
                     });
                 return deferred.promise;
-            },
-            cellSpecificOffsetDict: [
-                -24, -22, -20, -18, -16, -14, -12, -10, -8, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24
-            ]
+            }
+        };
+    })
+    .filter("dbStep32", function() {
+        var dict = [
+            -24, -22, -20, -18, -16, -14, -12, -10, -8, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24
+        ];
+        return function(input) {
+            return angular.isNumber(input) && input >= 0 && input < 32 ? dict[input] : input;
         };
     });
