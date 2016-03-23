762510817330bfe830f6b415927bef7182fd1d16
ouyh18 <ouyh18@189.cn>
2016-03-23 17:42:30 +0800

    2016-3-23 Add some neighbor cell classes.

---
+++ Lte.Parameters/Entities/Neighbor/EUtranRelation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lte.Parameters.Entities.Switch;
+
+namespace Lte.Parameters.Entities.Neighbor
+{
+    public class EUtranRelation : IMongoZte
+    {
+        public int eNodeB_Id { get; set; }
+
+        public string eNodeB_Name { get; set; }
+
+        public string lastModifedTime { get; set; }
+
+        public string iDate { get; set; }
+
+        public string parentLDN { get; set; }
+
+        public string description { get; set; }
+
+        public int resPRBDown { get; set; }
+
+        public int resPRBUp { get; set; }
+
+        public int overlapCoverage { get; set; }
+
+        public int shareCover { get; set; }
+
+        public int numRRCCntNumCov { get; set; }
+
+        public int lbIntraMeasureOffset { get; set; }
+
+        public int isX2HOAllowed { get; set; }
+
+        public string userLabel { get; set; }
+    }
+}
---
+++ Lte.Parameters/Entities/Neighbor/EutranIntraFreqNCell.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.Domain.Entities;
+using Lte.Parameters.Entities.Basic;
+using MongoDB.Bson;
+
+namespace Lte.Parameters.Entities.Neighbor
+{
+    public class EutranIntraFreqNCell : IEntity<ObjectId>, IHuaweiMongo
+    {
+        public ObjectId Id { get; set; }
+
+        public bool IsTransient()
+        {
+            return false;
+        }
+
+        public string iDate { get; set; }
+
+        public int eNodeB_Id { get; set; }
+
+        public string eNodeBId_Name { get; set; }
+
+        public int CtrlMode { get; set; }
+
+        public int CellMeasPriority { get; set; }
+
+        public string NeighbourCellName { get; set; }
+
+        public int AttachCellSwitch { get; set; }
+
+        public int NoHoFlag { get; set; }
+
+        public int CellId { get; set; }
+
+        public int LocalCellName { get; set; }
+
+        public int CellRangeExpansion { get; set; }
+
+        public int Mnc { get; set; }
+
+        public int Mcc { get; set; }
+
+        public int NCellClassLabel { get; set; }
+
+        public int CellQoffset { get; set; }
+
+        public int NoRmvFlag { get; set; }
+
+        public int eNodeBId { get; set; }
+
+        public int CellIndividualOffset { get; set; }
+
+        public int AnrFlag { get; set; }
+    }
+}
