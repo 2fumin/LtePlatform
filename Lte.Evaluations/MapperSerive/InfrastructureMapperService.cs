559543d1222a703fcc04f5daf096f34b9fcf552d
ouyh18 <ouyh18@189.cn>
2016-03-24 17:23:52 +0800

    2016-3-24 Construct NeighborCellMongo.

--- Lte.Evaluations/MapperSerive/InfrastructureMapperService.cs
+++ Lte.Evaluations/MapperSerive/InfrastructureMapperService.cs
@@ -27,6 +27,20 @@ namespace Lte.Evaluations.MapperSerive
                 .ForMember(d => d.DownTilt, opt => opt.MapFrom(s => s.ETilt + s.MTilt));
             Mapper.CreateMap<Cell, PciCell>();
             Mapper.CreateMap<NearestPciCell, NearestPciCellView>();
+
+            Mapper.CreateMap<EUtranRelationZte, NeighborCellMongo>()
+                .ForMember(d => d.CellId, opt => opt.MapFrom(s => s.eNodeB_Id))
+                .ForMember(d => d.IsAnrCreated, opt => opt.MapFrom(s => s.isAnrCreated == 1))
+                .ForMember(d => d.HandoffAllowed, opt => opt.MapFrom(s => s.isHOAllowed == 1))
+                .ForMember(d => d.RemovedAllowed, opt => opt.MapFrom(s => s.isRemoveAllowed == 1))
+                .ForMember(d => d.CellPriority, opt => opt.MapFrom(s => s.nCelPriority));
+
+            Mapper.CreateMap<EutranIntraFreqNCell, NeighborCellMongo>()
+                .ForMember(d => d.CellId, opt => opt.MapFrom(s => s.eNodeB_Id))
+                .ForMember(d => d.IsAnrCreated, opt => opt.MapFrom(s => s.AnrFlag > 0))
+                .ForMember(d => d.HandoffAllowed, opt => opt.MapFrom(s => s.NoHoFlag == 0))
+                .ForMember(d => d.RemovedAllowed, opt => opt.MapFrom(s => s.NoRmvFlag == 0))
+                .ForMember(d => d.CellPriority, opt => opt.MapFrom(s => s.CellMeasPriority));
         }
 
         public static void MapHoParametersService()
---
+++ Lte.Evaluations/ViewModels/Mr/NeighborCellMongo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lte.Domain.Regular.Attributes;
+
+namespace Lte.Evaluations.ViewModels.Mr
+{
+    [TypeDoc("来自MongoDB的LTE邻区关系视图")]
+    public class NeighborCellMongo
+    {
+        [MemberDoc("小区编号（对于LTE来说就是基站编号）")]
+        public int CellId { get; set; }
+
+        [MemberDoc("扇区编号")]
+        public byte SectorId { get; set; }
+
+        [MemberDoc("邻区小区编号")]
+        public int NeighborCellId { get; set; }
+
+        [MemberDoc("邻区扇区编号")]
+        public byte NeighborSectorId { get; set; }
+
+        [MemberDoc("PCI，便于查询邻区")]
+        public short NeighborPci { get; set; }
+
+        [MemberDoc("是否为ANR创建")]
+        public bool IsAnrCreated { get; set; }
+
+        [MemberDoc("是否允许切换")]
+        public bool HandoffAllowed { get; set; }
+
+        [MemberDoc("是否可以被ANR删除")]
+        public bool RemovedAllowed { get; set; }
+
+        [MemberDoc("小区测量优先级是否为高")]
+        public int CellPriority { get; set; }
+    }
+}
