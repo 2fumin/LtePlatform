082c7d40db55e3553b7e2440135a85625e7d44b3
ouyh18 <ouyh18@189.cn>
2016-03-27 19:55:19 +0800

    2016-3-27 Fix some bugs.

--- Lte.Evaluations/ViewModels/College/CollegeStat.cs
+++ Lte.Evaluations/ViewModels/College/CollegeStat.cs
@@ -3,40 +3,52 @@ using Lte.Parameters.Abstract;
 using Lte.Parameters.Abstract.College;
 using Lte.Parameters.Entities;
 using Lte.Parameters.Entities.College;
+using System.Runtime.Serialization;
 
 namespace Lte.Evaluations.ViewModels.College
 {
     [TypeDoc("校园网统计信息，主要包括用户数和有关的基础信息")]
+    [DataContract]
     public class CollegeStat
     {
         [MemberDoc("校园编号")]
+        [DataMember]
         public int Id { get; set; }
 
         [MemberDoc("校园名称")]
+        [DataMember]
         public string Name { get; private set; }
 
         [MemberDoc("用户到达数")]
+        [DataMember]
         public int ExpectedSubscribers { get; private set; }
 
         [MemberDoc("区域面积")]
+        [DataMember]
         public double Area { get; private set; }
 
         [MemberDoc("LTE基站总数")]
+        [DataMember]
         public int TotalLteENodebs { get; set; }
 
         [MemberDoc("LTE小区总数")]
+        [DataMember]
         public int TotalLteCells { get; set; }
 
         [MemberDoc("CDMA基站总数")]
+        [DataMember]
         public int TotalCdmaBts { get; set; }
 
         [MemberDoc("CDMA小区总数")]
+        [DataMember]
         public int TotalCdmaCells { get; set; }
 
         [MemberDoc("LTE室内分布总数")]
+        [DataMember]
         public int TotalLteIndoors { get; set; }
 
         [MemberDoc("CDMA室内分布总数")]
+        [DataMember]
         public int TotalCdmaIndoors { get; set; }
 
         public CollegeStat(ICollegeRepository repository, CollegeInfo info,
