b97a25d566d789363e1049b77217a7fcf40027fb
ouyh18 <ouyh18@189.cn>
2016-03-28 10:14:56 +0800

    2016-3-28 Show the college infos.

--- LtePlatform/Scripts/college/all.map.js
+++ LtePlatform/Scripts/college/all.map.js
@@ -2,7 +2,25 @@
     $scope.collegeInfo.url = $scope.rootPath + "map";
 
     var showCollegDialogs = function(college) {
-
+        var modalInstance = $uibModal.open({
+            animation: true,
+            templateUrl: '/appViews/College/Table/CollegeMapInfoBox.html',
+            controller: 'map.college.dialog',
+            size: 'sm',
+            resolve: {
+                dialogTitle: function () {
+                    return college.name + "-" + "基本信息";
+                },
+                college: function () {
+                    return college;
+                }
+            }
+        });
+        modalInstance.result.then(function (info) {
+            console.log(info);
+        }, function () {
+            $log.info('Modal dismissed at: ' + new Date());
+        });
     };
 
     baiduMapService.initializeMap("all-map", 11);
---
+++ LtePlatform/Scripts/college/map/college.dialog.js
@@ -0,0 +1,12 @@
+﻿app.controller('map.college.dialog', function ($scope, $uibModalInstance, college, dialogTitle) {
+    $scope.college = college;
+    $scope.dialogTitle = dialogTitle;
+
+    $scope.ok = function () {
+        $uibModalInstance.close($scope.college);
+    };
+
+    $scope.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
+});
