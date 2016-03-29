441000fb9aaaf59e1c1960a53b880bdf86c06a02
ouyh18 <ouyh18@189.cn>
2016-03-29 11:18:21 +0800

    2016-3-29 Add one selection in neighbor cell info box.

--- LtePlatform/Scripts/rutrace/map.neighbor.dialog.js
+++ LtePlatform/Scripts/rutrace/map.neighbor.dialog.js
@@ -1,6 +1,10 @@
 ﻿app.controller('map.neighbor.dialog', function ($scope, $uibModalInstance, neighbor, dialogTitle) {
     $scope.neighbor = neighbor;
     $scope.dialogTitle = dialogTitle;
+    $scope.parameter = {
+        options: ['基本参数', '同频切换', '异频切换'],
+        selected: '基本参数'
+    };
 
     $scope.ok = function () {
         $uibModalInstance.close($scope.neighbor);
