6ab440916a90a8cb0227dd9a2648b4cc271410af
ouyh18 <ouyh18@189.cn>
2016-03-30 11:11:51 +0800

    2016-3-30 Show mongo neighbors in parameters page.

--- LtePlatform/Scripts/parameters/details/cell.info.js
+++ LtePlatform/Scripts/parameters/details/cell.info.js
@@ -1,5 +1,5 @@
 ﻿app.controller("cell.info", function ($scope, $stateParams, networkElementService, cellHuaweiMongoService, intraFreqHoService,
-    interFreqHoService) {
+    interFreqHoService, neighborMongoService) {
     $scope.page.title = $stateParams.name + "-" + $stateParams.sectorId + "小区信息";
     $scope.isHuaweiCell = false;
     networkElementService.queryCellInfo($stateParams.eNodebId, $stateParams.sectorId).then(function (result) {
@@ -18,5 +18,8 @@
     });
     interFreqHoService.queryCellParameters($stateParams.eNodebId, $stateParams.sectorId).then(function (result) {
         $scope.interFreqHo = result;
+    });
+    neighborMongoService.queryNeighbors($stateParams.eNodebId, $stateParams.sectorId).then(function (result) {
+        $scope.mongoNeighbors = result;
     });
 });
