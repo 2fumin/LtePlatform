76fa2eb71bfbb9ea2375862401a2e445d2395090
ouyh18 <ouyh18@189.cn>
2016-03-30 14:36:15 +0800

    2016-3-30 Refactor...

--- LtePlatform/Scripts/parameters/details/cell.info.js
+++ LtePlatform/Scripts/parameters/details/cell.info.js
@@ -5,13 +5,8 @@
     networkElementService.queryCellInfo($stateParams.eNodebId, $stateParams.sectorId).then(function (result) {
         $scope.lteCellDetails = result;
     });
-    networkElementService.queryENodebInfo($stateParams.eNodebId).then(function(result) {
-        if (result.factory === '华为') {
-            $scope.isHuaweiCell = true;
-            cellHuaweiMongoService.queryCellParameters($stateParams.eNodebId, $stateParams.sectorId).then(function(info) {
-                $scope.cellMongo = info;
-            });
-        }
+    cellHuaweiMongoService.queryCellParameters($stateParams.eNodebId, $stateParams.sectorId).then(function(info) {
+        $scope.cellMongo = info;
     });
     intraFreqHoService.queryCellParameters($stateParams.eNodebId, $stateParams.sectorId).then(function(result) {
         $scope.intraFreqHo = result;
