d19ebe34880c0be55e957d7a840ef1424e78c4e0
ouyh18 <ouyh18@189.cn>
2016-03-30 21:31:58 +0800

    2016-3-30 Show Bts info.

---
+++ LtePlatform/Scripts/parameters/details/bts.info.js
@@ -0,0 +1,6 @@
+﻿app.controller("bts.info", function ($scope, $stateParams, networkElementService) {
+    $scope.page.title = $stateParams.name + "基础信息";
+    networkElementService.queryBtsInfo($stateParams.btsId).then(function (result) {
+        $scope.btsDetails = result;
+    });
+});
