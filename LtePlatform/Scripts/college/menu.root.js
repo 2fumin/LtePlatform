95eb9c95df207eafd05bfdb180be09da7810a9ba
ouyh18 <ouyh18@189.cn>
2016-04-02 22:19:15 +0800

    2016-4-2 Refactor college main page.

--- LtePlatform/Scripts/college/menu.root.js
+++ LtePlatform/Scripts/college/menu.root.js
@@ -3,23 +3,37 @@
     var rootUrl = "/College/Map#";
     $scope.menuItems = [
         {
-        	displayName: "校园网总览",
-            url: rootUrl + "/"
+            displayName: "覆盖情况",
+            tag: "coverage",
+            isActive: true,
+            subItems: [
+                {
+                    displayName: "校园网总览",
+                    url: rootUrl + "/"
+                }, {
+                    displayName: "基础信息查看",
+                    url: rootUrl + "/query"
+                }, {
+                    displayName: "校园覆盖",
+                    url: rootUrl + "/coverage"
+                }
+            ]
         }, {
-        	displayName: "基础信息查看",
-            url: rootUrl + "/query"
-        }, {
-        	displayName: "校园覆盖",
-            url: rootUrl + "/coverage"
-        }, {
-        	displayName: "测试报告",
-        	url: rootUrl + "/test"
-        }, {
-        	displayName: "指标报告",
-        	url: rootUrl + "/kpi"
-        }, {
-        	displayName: "精确覆盖",
-        	url: rootUrl + "/precise"
+            displayName: "日常管理",
+            tag: "management",
+            isActive: false,
+            subItems: [
+                {
+                    displayName: "测试报告",
+                    url: rootUrl + "/test"
+                }, {
+                    displayName: "指标报告",
+                    url: rootUrl + "/kpi"
+                }, {
+                    displayName: "精确覆盖",
+                    url: rootUrl + "/precise"
+                }
+            ]
         }
     ];
 });
