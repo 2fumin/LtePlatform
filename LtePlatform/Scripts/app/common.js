6d5a7b69e3a7c4eedf6c2fbbb680ef4162f32035
ouyh18 <ouyh18@189.cn>
2016-04-03 10:18:06 +0800

    2016-4-3 Refactor...

--- LtePlatform/Scripts/app/common.js
+++ LtePlatform/Scripts/app/common.js
@@ -1,135 +1,26 @@
-﻿
-window.common = (function() {
-    // 对Date的扩展，将 Date 转化为指定格式的String 
-    // 月(M)、日(d)、小时(h)、分(m)、秒(s)、季度(q) 可以用 1-2 个占位符， 
-    // 年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字) 
-    // 例子： 
-    // (new Date()).Format("yyyy-MM-dd hh:mm:ss.S") ==> 2006-07-02 08:09:04.423 
-    // (new Date()).Format("yyyy-M-d h:m:s.S")      ==> 2006-7-2 8:9:4.18 
-    Date.prototype.Format = function(fmt) { //author: meizz 
-        var o = {
-            "M+": this.getMonth() + 1, //月份 
-            "d+": this.getDate(), //日 
-            "h+": this.getHours(), //小时 
-            "m+": this.getMinutes(), //分 
-            "s+": this.getSeconds(), //秒 
-            "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
-            "S": this.getMilliseconds() //毫秒 
-        };
-        if (/(y+)/.test(fmt))
-            fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-        for (var k in o)
-            if (new RegExp("(" + k + ")").test(fmt))
-                fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
-        return fmt;
-    }
+﻿var app = angular.module('myApp', [
+    'college',
+    'college.map',
+    'handoff.parameters',
+    "highcharts-ng",
+    'huawei.mongo.parameters',
+    'kpi.basic',
+    'kpi.import',
+    'kpi.workitem',
+    'myApp.dumpInterference',
+    'myApp.kpi',
+    'myApp.parameters',
+    'myApp.region',
+    'myApp.url',
+    'ne.geometry',
+    'neighbor.mongo',
+    'ngRoute',
+    'parametersMap',
+    'parameters.chart',
+    "ui.bootstrap",
+    'ui.router'
+]);
 
-    Date.prototype.getDateFromToday = function(days) {
-        this.setDate(this.getDate() + days);
-        return this;
-    };
-
-    var common = {};
-
-    common.getFragment = function getFragment() {
-        if (window.location.hash.indexOf("#") === 0) {
-            return parseQueryString(window.location.hash.substr(1));
-        } else {
-            return {};
-        }
-    };
-
-    function parseQueryString(queryString) {
-        var data = {}, pair, separatorIndex, escapedKey, escapedValue, key, value;
-
-        if (queryString === null || queryString === undefined) {
-            return data;
-        }
-
-        var pairs = queryString.split("&");
-
-        for (var i = 0; i < pairs.length; i++) {
-            pair = pairs[i];
-            separatorIndex = pair.indexOf("=");
-
-            if (separatorIndex === -1) {
-                escapedKey = pair;
-                escapedValue = null;
-            } else {
-                escapedKey = pair.substr(0, separatorIndex);
-                escapedValue = pair.substr(separatorIndex + 1);
-            }
-
-            key = decodeURIComponent(escapedKey);
-            value = decodeURIComponent(escapedValue);
-
-            data[key] = value;
-        }
-
-        return data;
-    }
-
-    return common;
-})();
-
-var sendRequest = function (url, verb, data, successCallback, errorCallback, options) {
-    var requestOptions = options || {};
-    requestOptions.type = verb;
-    requestOptions.success = successCallback;
-    requestOptions.error = errorCallback;
-
-    if (!url || !verb) {
-        errorCallback(401, "URL and HTTP verb required!");
-    }
-
-    if (data) {
-        requestOptions.data = data;
-    }
-    $.ajax(url, requestOptions);
-}
-
-var setDefaultCallbacks = function (successCallback, errorCallback) {
-    $.ajaxSetup({
-        complete: function (jqXHR) {
-            if (jqXHR.status >= 200 && jqXHR.status < 300) {
-                successCallback(jqXHR.responseJSON);
-            } else {
-                errorCallback(jqXHR.status, jqXHR.statusText);
-            }
-        }
-    });
-}
-
-var setAjaxHeaders = function(requestHeaders) {
-    $.ajaxSetup({ headers: requestHeaders });
-};
-
-var lowerFirstLetter = function(str) {
-    return str.substring(0, 1).toLowerCase() +
-        str.substring(1);
-};
-
-var getErrorMessage = function (result) {
-    var error = result.error();
-    var responseText = error.responseText;
-    return "Status: " + error.status + "; Status Text: " + error.statusText + "; Message: " + responseText;
-};
-
-var showChartDialog = function(tag, chart) {
-    $(tag).dialog({
-        modal: true,
-        title: chart.title.text,
-        hide: 'slide',
-        width: 960,
-        height: 680,
-        buttons: {
-            '关闭': function() {
-                $(tag).dialog("close");
-            }
-        },
-        open: function() {
-            $(tag).html("");
-            $(tag).highcharts(chart.options);
-        }
-    });
-};
+app.run(function($rootScope) {
+    $rootScope.sideBarShown = true;
+});
