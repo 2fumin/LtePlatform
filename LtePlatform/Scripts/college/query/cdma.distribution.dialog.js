80c4285f79b36e24acc57efef2c86f092379f2a9
ouyh18 <ouyh18@189.cn>
2016-04-03 17:18:46 +0800

    2016-4-3 Show college distributions.

---
+++ LtePlatform/Scripts/college/query/cdma.distribution.dialog.js
@@ -0,0 +1,14 @@
+﻿app.controller('cdma.distribution.dialog', function ($scope, $uibModalInstance, collegeService, name, dialogTitle) {
+    $scope.dialogTitle = dialogTitle;
+    collegeService.queryCdmaDistributions(name).then(function(result) {
+        $scope.distributionList = result;
+    });
+
+    $scope.ok = function () {
+        $uibModalInstance.close($scope.distributionList);
+    };
+
+    $scope.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
+});
---
+++ LtePlatform/Scripts/college/query/lte.distribution.dialog.js
@@ -0,0 +1,14 @@
+﻿app.controller('lte.distribution.dialog', function ($scope, $uibModalInstance, collegeService, name, dialogTitle) {
+    $scope.dialogTitle = dialogTitle;
+    collegeService.queryLteDistributions(name).then(function(result) {
+        $scope.distributionList = result;
+    });
+
+    $scope.ok = function () {
+        $uibModalInstance.close($scope.distributionList);
+    };
+
+    $scope.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
+});
