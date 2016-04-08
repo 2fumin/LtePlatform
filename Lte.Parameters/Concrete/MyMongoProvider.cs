2f9c9c3c6148685d598908ff6095868bd497bb9e
ouyh18 <ouyh18@189.cn>
2016-04-08 09:17:45 +0800

    2016-4-8 Fix the bug in using MongoDb.

--- Lte.Parameters/Concrete/MyMongoProvider.cs
+++ Lte.Parameters/Concrete/MyMongoProvider.cs
@@ -11,34 +11,30 @@ namespace Lte.Parameters.Concrete
     public class MyMongoProvider : IMongoDatabaseProvider
     {
         private static readonly MongoClient Client = new MongoClient("mongodb://root:Abcdef9*@10.17.165.106:27017");
-        private static MongoDatabase _database;
 
         public MyMongoProvider(string databaseString)
         {
-            if (_database != null) return;
 #pragma warning disable 618
             var server = Client.GetServer();
 #pragma warning restore 618
-            _database = server.GetDatabase(databaseString);
+            Database = server.GetDatabase(databaseString);
         }
 
-        public MongoDatabase Database => _database;
+        public MongoDatabase Database { get; }
     }
 
     public class OuterMongoProvider : IMongoDatabaseProvider
     {
         private static readonly MongoClient Client = new MongoClient("mongodb://root:Abcdef9*@219.128.254.37:27017");
-        private static MongoDatabase _database;
 
         public OuterMongoProvider(string databaseString)
         {
-            if (_database != null) return;
 #pragma warning disable 618
             var server = Client.GetServer();
 #pragma warning restore 618
-            _database = server.GetDatabase(databaseString);
+            Database = server.GetDatabase(databaseString);
         }
 
-        public MongoDatabase Database => _database;
+        public MongoDatabase Database { get; }
     }
 }
