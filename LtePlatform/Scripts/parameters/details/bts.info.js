5e9af8a1964fbf1938b26fed4a7128b406ec26ad
ouyh18 <ouyh18@189.cn>
2016-04-09 00:17:52 +0800

    2016-4-9 Refactor...

--- LtePlatform/Scripts/parameters/details/bts.info.js
+++ LtePlatform/Scripts/parameters/details/bts.info.js
@@ -1,5 +1,5 @@
 ﻿app.controller("bts.info", function ($scope, $stateParams, networkElementService) {
-    $scope.page.title = $stateParams.name + "基础信息";
+    $scope.page.title = $stateParams.name + "CDMA基础信息";
     networkElementService.queryBtsInfo($stateParams.btsId).then(function (result) {
         $scope.btsDetails = result;
     });
--- LtePlatform/Scripts/parameters/details/menu.cdma.js
+++ LtePlatform/Scripts/parameters/details/menu.cdma.js
@@ -2,13 +2,16 @@
     $scope.menuTitle = $stateParams.name + "详细信息";
 
     menuItemService.updateMenuItem($scope.menuItems, 1,
-        $stateParams.name + "基础信息",
+        $stateParams.name + "CDMA基础信息",
         $scope.rootPath + "btsInfo" + "/" + $stateParams.btsId + "/" + $stateParams.name);
+    $scope.menu.accordions[$stateParams.name + "CDMA基础信息"] = true;
+
     networkElementService.queryCdmaSectorIds($stateParams.name).then(function (result) {
         angular.forEach(result, function (sectorId) {
             menuItemService.updateMenuItem($scope.menuItems, 1,
                 $stateParams.name + "-" + sectorId + "小区信息",
-                $scope.rootPath + "cdmaCellInfo" + "/" + $stateParams.btsId + "/" + $stateParams.name + "/" + sectorId);
+                $scope.rootPath + "cdmaCellInfo" + "/" + $stateParams.btsId + "/" + $stateParams.name + "/" + sectorId,
+                $stateParams.name + "CDMA基础信息");
         });
     });
 });
