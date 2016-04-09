987f0c8fdb64d727b66abfb367f93197f95cc66a
ouyh18ctc <ouyh19@189.cn>
2016-04-09 08:51:09 +0800

    2016-4-9 Modify connection.

--- Microsoft.Owin/phpApi/wandonglouyu.php
+++ Microsoft.Owin/phpApi/wandonglouyu.php
@@ -1,5 +1,5 @@
 <?php
-include_once("connect.php");
+include_once("zonghexinxiConnection.php");
 $sqlwd = "SELECT * FROM wandonglouyu where 是否5米地图区域='是' and 评估结果1='合格'";
 $resultwd = mysql_query($sqlwd);
 $recordcountwd = mysql_num_rows($resultwd);
