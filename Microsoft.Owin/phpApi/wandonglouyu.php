b0f0336c7b844c024c50a6496eb6e5570ecfa14e
ouyh18ctc <ouyh19@189.cn>
2016-04-09 10:26:38 +0800

    2016-4-9 Set the wandonglouyu.php to json output.

--- Microsoft.Owin/phpApi/wandonglouyu.php
+++ Microsoft.Owin/phpApi/wandonglouyu.php
@@ -1,11 +1,13 @@
 <?php
+header('content-type:application/json;charset=utf8');
 include_once("zonghexinxiConnection.php");
 $sqlwd = "SELECT * FROM wandonglouyu where 是否5米地图区域='是' and 评估结果1='合格'";
 $resultwd = mysql_query($sqlwd);
 $recordcountwd = mysql_num_rows($resultwd);
+$result=array();
 while ($rowswd = mysql_fetch_assoc($resultwd))
 {
-    echo $rowswd["经度"];
-    echo ",";
+    array_push($result, array ('longtitute'=>$rowswd["经度"],'lattitute'=>$rowswd["纬度"]));
 }
+echo json_encode($result);
 ?>
