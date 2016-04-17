61aa74bc351fde5d9e0b23718bef076fd3fc6cbe
ouyh18 <ouyh18@189.cn>
2016-04-17 21:19:24 +0800

    2016-4-17 Refactor...

--- LtePlatform/Controllers/Kpi/AlarmCountController.cs
+++ LtePlatform/Controllers/Kpi/AlarmCountController.cs
@@ -6,9 +6,11 @@ using System.Net.Http;
 using System.Web.Http;
 using Lte.Evaluations.DataService;
 using Lte.Parameters.Entities;
+using LtePlatform.Models;
 
 namespace LtePlatform.Controllers.Kpi
 {
+    [ApiControl("告警查询（测试）控制器")]
     public class AlarmCountController : ApiController
     {
         private readonly AlarmsService _service;
@@ -18,16 +20,19 @@ namespace LtePlatform.Controllers.Kpi
             _service = service;
         }
 
+        [ApiDoc("查询指定基站及日期范围内的告警数")]
         public Tuple<int, int> Get(int eNodebId, DateTime begin, DateTime end, int index)
         {
             return new Tuple<int, int>(index, _service.GetCounts(eNodebId, begin, end));
         }
 
+        [ApiDoc("查询所有告警列表")]
         public IEnumerable<AlarmStat> Get()
         {
             return _service.QueryAlarmStats();
         }
 
+        [ApiDoc("尝试导入一条告警记录")]
         public bool Get(int tryDump)
         {
             if (tryDump > 0) return _service.DumpOneStat();
