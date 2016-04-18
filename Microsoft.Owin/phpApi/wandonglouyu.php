55cb23995f6cafaa9c3c0bcb15a51346ab18c404
ouyh18ctc <ouyh19@189.cn>
2016-04-18 23:18:07 +0800

    2016-4-18 Update wandonglouyu.php

--- Microsoft.Owin/phpApi/wandonglouyu.php
+++ Microsoft.Owin/phpApi/wandonglouyu.php
@@ -7,7 +7,17 @@ $recordcountwd = mysql_num_rows($resultwd);
 $result=array();
 while ($rowswd = mysql_fetch_assoc($resultwd))
 {
-    array_push($result, array ('longtitute'=>$rowswd["经度"],'lattitute'=>$rowswd["纬度"]));
+    array_push($result, array (
+        'longtitute'=>$rowswd["经度"],
+        'lattitute'=>$rowswd["纬度"],
+        'name'=>urlencode($rowswd["楼群名称"]),
+        'type'=>$rowswd["类型"],
+        'rsrpCoverage'=>$rowswd["RSRP达标率"],
+        'evaluationResult'=>urlencode($rowswd["评估结果1"]),
+        'testResult'=>$rowswd["测试评估"],
+        'alternativeResult'=>$rowswd["评估结果"],
+        'coverageRate'=>$rowswd["覆盖率"]
+        ));
 }
-echo json_encode($result);
+echo urldecode(json_encode($result));
 ?>
