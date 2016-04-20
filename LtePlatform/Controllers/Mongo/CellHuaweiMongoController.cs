4a0bac1881b77840737d96dbb4b31dd440d64225
ouyh18ctc <ouyh19@189.cn>
2016-04-20 21:19:06 +0800

    2016-4-20 Add some comments...

--- LtePlatform/Controllers/Mongo/CellHuaweiMongoController.cs
+++ LtePlatform/Controllers/Mongo/CellHuaweiMongoController.cs
@@ -6,9 +6,11 @@ using System.Net.Http;
 using System.Web.Http;
 using Lte.Evaluations.DataService.Basic;
 using Lte.Parameters.Entities.Basic;
+using LtePlatform.Models;
 
 namespace LtePlatform.Controllers.Mongo
 {
+    [ApiControl("从MongoDB数据库中查询小区基本信息（不限于华为）")]
     public class CellHuaweiMongoController : ApiController
     {
         private readonly CellHuaweiMongoService _service;
@@ -19,12 +21,19 @@ namespace LtePlatform.Controllers.Mongo
         }
 
         [HttpGet]
+        [ApiDoc("查询指定小区的基本信息")]
+        [ApiParameterDoc("eNodebId", "基站编号")]
+        [ApiParameterDoc("sectorId", "扇区编号")]
+        [ApiResponse("指定小区的基本信息")]
         public CellHuaweiMongo Get(int eNodebId, byte sectorId)
         {
             return _service.QueryRecentCellInfo(eNodebId, sectorId);
         }
 
         [HttpGet]
+        [ApiDoc("查询指定基站下带的华为本地小区标识定义")]
+        [ApiParameterDoc("eNodebId", "基站编号")]
+        [ApiResponse("指定基站下带的华为本地小区标识定义")]
         public HuaweiLocalCellDef Get(int eNodebId)
         {
             return _service.QueryLocalCellDef(eNodebId);
--- LtePlatform/Controllers/Mr/CellStasticController.cs
+++ LtePlatform/Controllers/Mr/CellStasticController.cs
@@ -6,9 +6,11 @@ using System.Net.Http;
 using System.Web.Http;
 using Lte.Evaluations.DataService.Mr;
 using Lte.Evaluations.ViewModels.Mr;
+using LtePlatform.Models;
 
 namespace LtePlatform.Controllers.Mr
 {
+    [ApiControl("从MongoDB数据库中查询小区干扰信息统计")]
     public class CellStasticController : ApiController
     {
         private readonly CellStasticService _service;
@@ -19,6 +21,12 @@ namespace LtePlatform.Controllers.Mr
         }
 
         [HttpGet]
+        [ApiDoc("查询指定日期范围内的单个小区聚合小区干扰信息统计")]
+        [ApiParameterDoc("eNodebId", "小区基站编号")]
+        [ApiParameterDoc("pci", "小区PCI（由于MR中不记录扇区编号，必须用PCI进行查询）")]
+        [ApiParameterDoc("begin", "开始日期")]
+        [ApiParameterDoc("end", "结束日期")]
+        [ApiResponse("单个小区聚合小区干扰信息统计")]
         public CellStasticView Get(int eNodebId, short pci, DateTime begin, DateTime end)
         {
             return _service.QueryDateSpanAverageStat(eNodebId, pci, begin, end);
