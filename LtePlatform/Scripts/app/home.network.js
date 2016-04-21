e0366f188b8a40d389e68e7625d7f1f15634b01d
ouyh18ctc <ouyh19@189.cn>
2016-04-21 22:37:46 +0800

    2016-4-20 Show LTE network stats in the home page.

---
+++ LtePlatform/Scripts/app/home.network.js
@@ -0,0 +1,6 @@
+﻿app.controller("home.network", function ($scope, appRegionService, parametersChartService) {
+    appRegionService.queryDistrictInfrastructures($scope.city.selected || '佛山').then(function (result) {
+        $("#cityLteENodebConfig").highcharts(parametersChartService.getDistrictLteENodebPieOptions(result.slice(0, result.length - 1),
+            $scope.city.selected || '佛山'));
+    });
+});
