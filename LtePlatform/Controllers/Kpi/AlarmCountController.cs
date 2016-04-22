ca0466b234243d7f0b4557b1842d9a036fb1bbfe
ouyh18ctc <ouyh19@189.cn>
2016-04-22 10:38:53 +0800

    2016-4-22 Refactor...

--- LtePlatform/Controllers/Kpi/AlarmCountController.cs
+++ LtePlatform/Controllers/Kpi/AlarmCountController.cs
@@ -21,18 +21,26 @@ namespace LtePlatform.Controllers.Kpi
         }
 
         [ApiDoc("查询指定基站及日期范围内的告警数")]
+        [ApiParameterDoc("eNodebId", "基站编号")]
+        [ApiParameterDoc("begin", "开始日期")]
+        [ApiParameterDoc("end", "结束日期")]
+        [ApiParameterDoc("index", "索引号，传递参数用，无实际含义")]
+        [ApiResponse("告警数")]
         public Tuple<int, int> Get(int eNodebId, DateTime begin, DateTime end, int index)
         {
             return new Tuple<int, int>(index, _service.GetCounts(eNodebId, begin, end));
         }
 
         [ApiDoc("查询所有告警列表")]
+        [ApiResponse("所有告警列表")]
         public IEnumerable<AlarmStat> Get()
         {
             return _service.QueryAlarmStats();
         }
 
         [ApiDoc("尝试导入一条告警记录")]
+        [ApiParameterDoc("tryDump", "标志参数，大于0时表示尝试导入")]
+        [ApiResponse("导入是否成功")]
         public bool Get(int tryDump)
         {
             if (tryDump > 0) return _service.DumpOneStat();
