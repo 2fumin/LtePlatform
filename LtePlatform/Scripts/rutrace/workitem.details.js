ab2e96abfa7b0e8ff1d3c0b46fe0b7ea53244797
ouyh18ctc <ouyh19@189.cn>
2016-04-26 16:28:03 +0800

    2016-4-26 Refactor...

--- LtePlatform/Scripts/rutrace/workitem.details.js
+++ LtePlatform/Scripts/rutrace/workitem.details.js
@@ -1,19 +1,25 @@
 ﻿app.controller("workitem.details", function($scope, $routeParams, workitemService, workItemDialog) {
-    if ($scope.viewData.workItems === undefined || $scope.viewData.workItems.length === 0) {
+    $scope.queryWorkItems = function () {
         workitemService.querySingleItem($routeParams.number).then(function (result) {
             $scope.currentView = result;
             $scope.platformInfos = workItemDialog.calculatePlatformInfo($scope.currentView.comments);
             $scope.feedbackInfos = workItemDialog.calculateFeedbackInfo($scope.currentView.feedbackContents);
         });
-    } else {
-        $scope.viewItems = $scope.viewData.workItems;
-        for (var i = 0; i < $scope.viewData.workItems.length; i++) {
-            if ($scope.viewData.workItems[i].serialNumber === $routeParams.number) {
-                $scope.currentView = $scope.viewData.workItems[i];
-                $scope.platformInfos = workItemDialog.calculatePlatformInfo($scope.currentView.comments);
-                $scope.feedbackInfos = workItemDialog.calculateFeedbackInfo($scope.currentView.feedbackContents);
-                break;
+    };
+    $scope.feedback = function (view) {
+        workItemDialog.feedback(view, $scope.queryWorkItems);
+    };
+    if ($scope.viewData.workItems === undefined || $scope.viewData.workItems.length === 0) {
+        $scope.queryWorkItems();
+        } else {
+            $scope.viewItems = $scope.viewData.workItems;
+            for (var i = 0; i < $scope.viewData.workItems.length; i++) {
+                if ($scope.viewData.workItems[i].serialNumber === $routeParams.number) {
+                    $scope.currentView = $scope.viewData.workItems[i];
+                    $scope.platformInfos = workItemDialog.calculatePlatformInfo($scope.currentView.comments);
+                    $scope.feedbackInfos = workItemDialog.calculateFeedbackInfo($scope.currentView.feedbackContents);
+                    break;
+                }
             }
         }
-    }
 });
--- LtePlatform/Scripts/rutrace/workitems.controller.js
+++ LtePlatform/Scripts/rutrace/workitems.controller.js
@@ -1,13 +1,18 @@
 ﻿app.controller("rutrace.workitems", function ($scope, $routeParams, workitemService, workItemDialog) {
     $scope.page.title = "TOP小区工单历史";
-
-    workitemService.queryByCellId($routeParams.cellId, $routeParams.sectorId).then(function(result) {
-        $scope.viewItems = result;
-        $scope.viewData.workItems = result;
-        if (result.length > 0) {
-            $scope.currentView = result[0];
-            $scope.platformInfos = workItemDialog.calculatePlatformInfo($scope.currentView.comments);
-            $scope.feedbackInfos = workItemDialog.calculateFeedbackInfo($scope.currentView.feedbackContents);
-        }
-    });
+    $scope.queryWorkItems = function () {
+        workitemService.queryByCellId($routeParams.cellId, $routeParams.sectorId).then(function (result) {
+            $scope.viewItems = result;
+            $scope.viewData.workItems = result;
+            if (result.length > 0) {
+                $scope.currentView = result[0];
+                $scope.platformInfos = workItemDialog.calculatePlatformInfo($scope.currentView.comments);
+                $scope.feedbackInfos = workItemDialog.calculateFeedbackInfo($scope.currentView.feedbackContents);
+            }
+        });
+    };
+    $scope.feedback = function (view) {
+        workItemDialog.feedback(view, $scope.queryWorkItems);
+    };
+    $scope.queryWorkItems();
 });
