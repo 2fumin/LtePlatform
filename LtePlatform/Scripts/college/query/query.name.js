ff09df11cfa5d800d3e6dd96bd01c1c198ed68cf
ouyh18 <ouyh18@189.cn>
2016-04-27 21:44:15 +0800

    2016-4-27 Add college ENodeb Table.

--- LtePlatform/Scripts/college/query/query.name.js
+++ LtePlatform/Scripts/college/query/query.name.js
@@ -1,4 +1,7 @@
-﻿app.controller("query.name", function ($scope, $stateParams) {
+﻿app.controller("query.name", function ($scope, $stateParams, collegeService) {
     $scope.collegeInfo.url = $scope.rootPath + "query";
     $scope.collegeName = $stateParams.name;
+    collegeService.queryENodebs($scope.collegeName).then(function (eNodebs) {
+        $scope.eNodebList = eNodebs;
+    });
 });
