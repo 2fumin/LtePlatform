6d7ee70c907f20ccb9c3242bb9babb7ef0b8a24c
ouyh18 <ouyh18@189.cn>
2016-05-01 06:44:02 +0800

    2016-5-1 Refactor...

--- System.Web.Http.Owin/OwinConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace System.Web.Http.Owin
-{
-    internal static class OwinConstants
-    {
-        public const string ClientCertifiateKey = "ssl.ClientCertificate";
-        public const string IsLocalKey = "server.IsLocal";
-    }
-}
