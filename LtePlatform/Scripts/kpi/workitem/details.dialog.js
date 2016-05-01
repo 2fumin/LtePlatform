7353462a7788eebb3d393c9d728366d33c58aa09
ouyh18 <ouyh18@189.cn>
2016-05-01 17:44:40 +0800

    2016-5-1 Refactor...

---
+++ LtePlatform/Scripts/kpi/workitem/details.dialog.js
@@ -0,0 +1,16 @@
+﻿app.controller('workitem.details.dialog', function ($scope, $uibModalInstance, input, dialogTitle, workItemDialog) {
+    $scope.currentView = input;
+    $scope.dialogTitle = dialogTitle;
+    $scope.message = "";
+    $scope.platformInfos = workItemDialog.calculatePlatformInfo($scope.currentView.comments);
+    $scope.feedbackInfos = workItemDialog.calculateFeedbackInfo($scope.currentView.feedbackContents);
+    $scope.preventChangeParentView = true;
+
+    $scope.ok = function () {
+        $uibModalInstance.close($scope.message);
+    };
+
+    $scope.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
+});
