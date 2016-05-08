0e1af20cd70b7dbae8b929a98147806ceb911ed9
ouyh18 <ouyh18@189.cn>
2016-05-08 22:08:11 +0800

    2016-5-8 Refactor...

--- LtePlatform/Scripts/rutrace/interference/source.dialog.js
+++ LtePlatform/Scripts/rutrace/interference/source.dialog.js
@@ -1,7 +1,56 @@
-﻿app.controller('interference.source.dialog', function ($scope, $uibModalInstance, dialogTitle, eNodebId, sectorId) {
+﻿app.controller('interference.source.dialog', function ($scope, $uibModalInstance, dialogTitle, eNodebId, sectorId,
+    topPreciseService, neighborMongoService) {
     $scope.dialogTitle = dialogTitle;
-    $scope.eNodebId = eNodebId;
-    $scope.sectorId = sectorId;
+    var lastWeek = new Date();
+    lastWeek.setDate(lastWeek.getDate() - 7);
+    $scope.beginDate = {
+        value: lastWeek,
+        opened: false
+    };
+    $scope.endDate = {
+        value: new Date(),
+        opened: false
+    };
+    var options = [
+        {
+            name: "模3干扰数",
+            value: "mod3Interferences"
+        }, {
+            name: "模6干扰数",
+            value: "mod6Interferences"
+        }, {
+            name: "6dB干扰数",
+            value: "overInterferences6Db"
+        }, {
+            name: "10dB干扰数",
+            value: "overInterferences10Db"
+        }, {
+            name: "总干扰水平",
+            value: "interferenceLevel"
+        }
+    ];
+    $scope.orderPolicy = {
+        options: options,
+        selected: options[4].value
+    };
+
+    $scope.showInterference = function () {
+        $scope.interferenceCells = [];
+
+        topPreciseService.queryInterferenceNeighbor($scope.beginDate.value, $scope.endDate.value,
+            eNodebId, sectorId).then(function (result) {
+                angular.forEach(result, function (cell) {
+                    for (var i = 0; i < $scope.mongoNeighbors.length; i++) {
+                        var neighbor = $scope.mongoNeighbors[i];
+                        if (neighbor.neighborPci === cell.destPci) {
+                            cell.isMongoNeighbor = true;
+                            break;
+                        }
+                    }
+                });
+                $scope.interferenceCells = result;
+            });
+    };
 
     $scope.ok = function () {
         $uibModalInstance.close('已处理');
@@ -10,4 +59,9 @@
     $scope.cancel = function () {
         $uibModalInstance.dismiss('cancel');
     };
+
+    neighborMongoService.queryNeighbors(eNodebId, sectorId).then(function (result) {
+        $scope.mongoNeighbors = result;
+        $scope.showInterference();
+    });
 });
